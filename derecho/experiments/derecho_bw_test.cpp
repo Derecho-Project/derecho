c856acd180b96bba9bdfa9e7cff6b289f5ad4239
Sagar Jha <sjsagarjha3@gmail.com>
2017-06-12 19:51:26 -0400

    Let exit rule unless leave is debugged

--- derecho/experiments/derecho_bw_test.cpp
+++ derecho/experiments/derecho_bw_test.cpp
@@ -49,14 +49,14 @@ int main(int argc, char *argv[]) {
         pthread_setname_np(pthread_self(), "bw_test");
         srand(time(NULL));
 
-    uint32_t server_rank = 0;
-    uint32_t node_id;
-    uint32_t num_nodes;
+        uint32_t server_rank = 0;
+        uint32_t node_id;
+        uint32_t num_nodes;
 
         map<uint32_t, std::string> node_addresses;
 
-    rdmc::query_addresses(node_addresses, node_id);
-    num_nodes = node_addresses.size();
+        rdmc::query_addresses(node_addresses, node_id);
+        num_nodes = node_addresses.size();
 
         vector<uint32_t> members(num_nodes);
         for(uint32_t i = 0; i < num_nodes; ++i) {
@@ -148,11 +148,57 @@ int main(int argc, char *argv[]) {
 
         cout << "Finished constructing/joining ManagedGroup" << endl;
 
-    auto membership_function = [num_senders_selector, mode, num_nodes](const View &curr_view, int &next_unassigned_rank, bool previous_was_successful) {
-        subgroup_shard_layout_t subgroup_vector(1);
-        auto num_members = curr_view.members.size();
-        if(num_members < num_nodes) {
-            throw derecho::subgroup_provisioning_exception();
+        auto membership_function = [num_senders_selector, mode, num_nodes](const View &curr_view, int &next_unassigned_rank, bool previous_was_successful) {
+            subgroup_shard_layout_t subgroup_vector(1);
+            auto num_members = curr_view.members.size();
+            if(num_members < num_nodes) {
+                throw derecho::subgroup_provisioning_exception();
+            }
+            auto members_order = managed_group->get_members();
+            cout << "The order of members is :" << endl;
+            for(auto id : members_order) {
+                cout << id << " ";
+            }
+            cout << endl;
+
+            auto send_all = [&]() {
+                RawSubgroup &group_as_subgroup = managed_group->get_subgroup<RawObject>();
+                for(int i = 0; i < num_messages; ++i) {
+                    // cout << "Asking for a buffer" << endl;
+                    char *buf = group_as_subgroup.get_sendbuffer_ptr(max_msg_size, send_medium);
+                    while(!buf) {
+                        buf = group_as_subgroup.get_sendbuffer_ptr(max_msg_size, send_medium);
+                    }
+                    buf[0] = '0' + i;
+                    // cout << "Obtained a buffer, sending" << endl;
+                    group_as_subgroup.send();
+                }
+                subgroup_vector[0].emplace_back(curr_view.make_subview(curr_view.members, mode, is_sender));
+            } next_unassigned_rank
+                    = curr_view.members.size();
+            return subgroup_vector;
+        };
+
+        derecho::SubgroupInfo one_raw_group({{std::type_index(typeid(RawObject)), membership_function}});
+
+        std::unique_ptr<derecho::Group<>> managed_group;
+        if(node_id == server_rank) {
+            managed_group = std::make_unique<derecho::Group<>>(
+                    node_id, node_addresses[node_id],
+                    derecho::CallbackSet{stability_callback, nullptr},
+                    one_raw_group,
+                    derecho::DerechoParams{max_msg_size, block_size, std::string(), window_size});
+        } else {
+            managed_group = std::make_unique<derecho::Group<>>(
+                    node_id, node_addresses[node_id],
+                    node_addresses[server_rank],
+                    derecho::CallbackSet{stability_callback, nullptr},
+                    one_raw_group);
+        }
+
+        cout << "Finished constructing/joining ManagedGroup" << endl;
+
+        while(managed_group->get_members().size() < num_nodes) {
         }
         auto members_order = managed_group->get_members();
         cout << "The order of members is :" << endl;
@@ -160,6 +206,10 @@ int main(int argc, char *argv[]) {
             cout << id << " ";
         }
         cout << endl;
+        uint32_t my_rank = 0;
+        while(members_order[my_rank] != node_id) {
+            my_rank++;
+        }
 
         auto send_all = [&]() {
             RawSubgroup &group_as_subgroup = managed_group->get_subgroup<RawObject>();
@@ -169,99 +219,51 @@ int main(int argc, char *argv[]) {
                 while(!buf) {
                     buf = group_as_subgroup.get_sendbuffer_ptr(max_msg_size, send_medium);
                 }
-                buf[0] = '0' + i;
-                // cout << "Obtained a buffer, sending" << endl;
-                group_as_subgroup.send();
             }
-            subgroup_vector[0].emplace_back(curr_view.make_subview(curr_view.members, mode, is_sender));
-        }
-        next_unassigned_rank = curr_view.members.size();
-        return subgroup_vector;
-    };
-
-    derecho::SubgroupInfo one_raw_group({{std::type_index(typeid(RawObject)), membership_function}});
-    
-    std::unique_ptr<derecho::Group<>> managed_group;
-    if(node_id == server_rank) {
-        managed_group = std::make_unique<derecho::Group<>>(
-                node_id, node_addresses[node_id],
-                derecho::CallbackSet{stability_callback, nullptr},
-                one_raw_group,
-                derecho::DerechoParams{max_msg_size, block_size, std::string(), window_size});
-    } else {
-        managed_group = std::make_unique<derecho::Group<>>(
-                node_id, node_addresses[node_id],
-                node_addresses[server_rank],
-                derecho::CallbackSet{stability_callback, nullptr},
-                one_raw_group);
-    }
-
-    cout << "Finished constructing/joining ManagedGroup" << endl;
-
-    while(managed_group->get_members().size() < num_nodes) {
-    }
-    auto members_order = managed_group->get_members();
-    cout << "The order of members is :" << endl;
-    for(auto id : members_order) {
-        cout << id << " ";
-    }
-    cout << endl;
-    uint32_t my_rank = 0;
-    while(members_order[my_rank] != node_id) {
-        my_rank++;
-    }
-
-    auto send_all = [&]() {
-        RawSubgroup &group_as_subgroup = managed_group->get_subgroup<RawObject>();
-        for(int i = 0; i < num_messages; ++i) {
-            // cout << "Asking for a buffer" << endl;
-            char *buf = group_as_subgroup.get_sendbuffer_ptr(max_msg_size, send_medium);
-            while(!buf) {
-                buf = group_as_subgroup.get_sendbuffer_ptr(max_msg_size, send_medium);
-            }
-        }
-    };
-
-    managed_group->barrier_sync();
-    struct timespec start_time;
-    // start timer
-    clock_gettime(CLOCK_REALTIME, &start_time);
-    if(num_senders_selector == 0) {
-        send_all();
-    } else if(num_senders_selector == 1) {
-        if(my_rank > (num_nodes - 1) / 2) {
-            send_all();
-        }
-    } else {
-        if(my_rank == num_nodes - 1) {
-            send_all();
-        }
-    }
-    while(!done) {
-    }
-    struct timespec end_time;
-    clock_gettime(CLOCK_REALTIME, &end_time);
-    long long int nanoseconds_elapsed = (end_time.tv_sec - start_time.tv_sec) * (long long int)1e9 + (end_time.tv_nsec - start_time.tv_nsec);
-    double bw;
-    if(num_senders_selector == 0) {
-        bw = (max_msg_size * num_messages * num_nodes + 0.0) / nanoseconds_elapsed;
-    } else if(num_senders_selector == 1) {
-        bw = (max_msg_size * num_messages * (num_nodes / 2) + 0.0) / nanoseconds_elapsed;
-    } else {
-        bw = (max_msg_size * num_messages + 0.0) / nanoseconds_elapsed;
-    }
-    double avg_bw = aggregate_bandwidth(members, node_id, bw);
-    if(node_id == 0) {
-        log_results(exp_result{num_nodes, num_senders_selector, max_msg_size,
-                               window_size, num_messages, send_medium,
-                               raw_mode, avg_bw},
-                    "data_derecho_bw");
-    }
+        };
 
         managed_group->barrier_sync();
-        managed_group->leave();
-        sst::verbs_destroy();
+        struct timespec start_time;
+        // start timer
+        clock_gettime(CLOCK_REALTIME, &start_time);
+        if(num_senders_selector == 0) {
+            send_all();
+        } else if(num_senders_selector == 1) {
+            if(my_rank > (num_nodes - 1) / 2) {
+                send_all();
+            }
+        } else {
+            if(my_rank == num_nodes - 1) {
+                send_all();
+            }
+        }
+        while(!done) {
+        }
+        struct timespec end_time;
+        clock_gettime(CLOCK_REALTIME, &end_time);
+        long long int nanoseconds_elapsed = (end_time.tv_sec - start_time.tv_sec) * (long long int)1e9 + (end_time.tv_nsec - start_time.tv_nsec);
+        double bw;
+        if(num_senders_selector == 0) {
+            bw = (max_msg_size * num_messages * num_nodes + 0.0) / nanoseconds_elapsed;
+        } else if(num_senders_selector == 1) {
+            bw = (max_msg_size * num_messages * (num_nodes / 2) + 0.0) / nanoseconds_elapsed;
+        } else {
+            bw = (max_msg_size * num_messages + 0.0) / nanoseconds_elapsed;
+        }
+        double avg_bw = aggregate_bandwidth(members, node_id, bw);
+        if(node_id == 0) {
+            log_results(exp_result{num_nodes, num_senders_selector, max_msg_size,
+                                   window_size, num_messages, send_medium,
+                                   raw_mode, avg_bw},
+                        "data_derecho_bw");
+        }
+
+        managed_group->barrier_sync();
+        // managed_group->leave();
+        // sst::verbs_destroy();
         cout << "Finished destroying managed_group" << endl;
+        // for now
+        exit(0);
     } catch(const std::exception &e) {
         cout << "Exception in main: " << e.what() << endl;
         cout << "main shutting down" << endl;
