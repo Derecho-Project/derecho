23ba1bfac14de2d817a8c6a8273b397c31b16869
Sagar Jha <sjsagarjha3@gmail.com>
2017-06-16 16:44:46 -0400

    write_avg_time now computes times for every pair of nodes
    It also just samples the size in multiples of 2 going from 1 to 16KB

---
+++ sst/experiments/compute_nodes_list.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+std::list<std::pair<int32_t, int32_t>> compute_sequence(int32_t start, int32_t end);
+std::vector<int32_t> compute_nodes_list(int32_t my_id, int32_t num_nodes);
--- sst/experiments/write_avg_time.cpp
+++ sst/experiments/write_avg_time.cpp
@@ -1,15 +1,10 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <vector>
 
+#include "compute_nodes_list.h"
 #include "sst/verbs.h"
-
-using std::ofstream;
-using std::map;
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
 
 using namespace sst;
 
@@ -17,56 +12,93 @@ using namespace sst;
 long long int num_reruns = 10000;
 
 int main() {
-    ofstream fout;
-    fout.open("data_write_avg_time", ofstream::app);
+    std::ofstream fout;
+    fout.open("data_write_avg_time", std::ofstream::app);
+    std::vector<int> size_arr = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384};
 
-    cout << "FOR THIS EXPERIMENT TO WORK, DO NOT START THE POLLING THREAD!!!" << endl;
+    std::cout << "FOR THIS EXPERIMENT TO WORK, DO NOT START THE POLLING THREAD!!!" << std::endl;
 
     // input number of nodes and the local node id
     int num_nodes, node_rank;
-    cin >> node_rank;
-    cin >> num_nodes;
+    std::cin >> node_rank;
+    std::cin >> num_nodes;
 
     // input the ip addresses
-    map<uint32_t, string> ip_addrs;
+    std::map<uint32_t, std::string> ip_addrs;
     for(int i = 0; i < num_nodes; ++i) {
-        cin >> ip_addrs[i];
+        std::cin >> ip_addrs[i];
     }
 
     // initialize the rdma resources
     verbs_initialize(ip_addrs, node_rank);
 
-    int r_index = num_nodes - 1 - node_rank;
-    for(int size = 1; size <= 10000; ++size) {
-        // create buffer for write and read
-        char *write_buf, *read_buf;
-        write_buf = (char *)malloc(size);
-        read_buf = (char *)malloc(size);
+    auto nodes_list = compute_nodes_list(node_rank, num_nodes);
+    for(auto remote_rank : nodes_list) {
+        sync(remote_rank);
+        for(int size : size_arr) {
+            // create buffer for write and read
+            char *write_buf, *read_buf;
+            write_buf = (char *)malloc(size);
+            read_buf = (char *)malloc(size);
 
-        resources res(r_index, read_buf, write_buf, size, size);
+            resources res(remote_rank, read_buf, write_buf, size, size);
 
-        // start the timing experiment
-        struct timespec start_time;
-        struct timespec end_time;
-        long long int nanoseconds_elapsed;
+            // start the timing experiment
+            struct timespec start_time;
+            struct timespec end_time;
+            long long int nanoseconds_elapsed;
 
-        if(node_rank == 0) {
-            clock_gettime(CLOCK_REALTIME, &start_time);
-            for(int i = 0; i < num_reruns; ++i) {
-                // write the entire buffer
-                res.post_remote_write_with_completion(0, size);
-                // poll for completion
-                verbs_poll_completion();
+            if(node_rank < remote_rank) {
+                clock_gettime(CLOCK_REALTIME, &start_time);
+                for(int i = 0; i < num_reruns; ++i) {
+                    // write the entire buffer
+                    res.post_remote_write_with_completion(0, size);
+                    // poll for completion
+                    verbs_poll_completion();
+                }
+                clock_gettime(CLOCK_REALTIME, &end_time);
+                nanoseconds_elapsed = (end_time.tv_sec - start_time.tv_sec) * 1000000000 + (end_time.tv_nsec - start_time.tv_nsec);
+                fout << node_rank << " " << remote_rank << " " << size << " " << (nanoseconds_elapsed + 0.0) / (1000 * num_reruns) << std::endl;
+                free(write_buf);
+                free(read_buf);
             }
-            clock_gettime(CLOCK_REALTIME, &end_time);
-            nanoseconds_elapsed = (end_time.tv_sec - start_time.tv_sec) * 1000000000 + (end_time.tv_nsec - start_time.tv_nsec);
-            fout << size << " " << (nanoseconds_elapsed + 0.0) / (1000 * num_reruns) << endl;
-            free(write_buf);
-            free(read_buf);
+            sync(remote_rank);
         }
-        sync(r_index);
+        for(int size : size_arr) {
+            // create buffer for write and read
+            char *write_buf, *read_buf;
+            write_buf = (char *)malloc(size);
+            read_buf = (char *)malloc(size);
+
+            resources res(remote_rank, read_buf, write_buf, size, size);
+
+            // start the timing experiment
+            struct timespec start_time;
+            struct timespec end_time;
+            long long int nanoseconds_elapsed;
+
+            if(remote_rank < node_rank) {
+                clock_gettime(CLOCK_REALTIME, &start_time);
+                for(int i = 0; i < num_reruns; ++i) {
+                    // write the entire buffer
+                    res.post_remote_write_with_completion(0, size);
+                    // poll for completion
+                    verbs_poll_completion();
+                }
+                clock_gettime(CLOCK_REALTIME, &end_time);
+                nanoseconds_elapsed = (end_time.tv_sec - start_time.tv_sec) * 1000000000 + (end_time.tv_nsec - start_time.tv_nsec);
+                fout << node_rank << " " << remote_rank << " " << size << " " << (nanoseconds_elapsed + 0.0) / (1000 * num_reruns) << std::endl;
+                free(write_buf);
+                free(read_buf);
+            }
+            sync(remote_rank);
+        }
     }
-    sync(r_index);
+    for (int i = 0; i < num_nodes; ++i) {
+      if (i != node_rank) {
+	sync(i);
+      }
+    }
     fout.close();
     return 0;
 }
