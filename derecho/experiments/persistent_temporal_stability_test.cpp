6354f10be6e5de077e70a8d47a02bf4a15022f63
Weijia <ws393@cornell.edu>
2017-09-11 15:43:17 -0400

    added persistent/volatile query test.

--- derecho/experiments/persistent_temporal_stability_test.cpp
+++ derecho/experiments/persistent_temporal_stability_test.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <string>
 #include <pthread.h>
-#include <stdlib.h>
 
 #include "block_size.h"
 #include "derecho/derecho.h"
@@ -38,9 +37,6 @@ struct Bytes : public mutils::ByteRepresentable{
                 bytes = new char[s];
                 memcpy(bytes,b,s);
             }
-        }
-        Bytes(const Bytes & rhs)
-          :Bytes(rhs.bytes, rhs.size){
         }
         Bytes() {
           bytes = nullptr;
@@ -102,6 +98,7 @@ struct Bytes : public mutils::ByteRepresentable{
         static context_ptr<Bytes> from_bytes_noalloc(mutils::DeserializationManager *, const char * const v)  {
                 return context_ptr<Bytes>{new Bytes(v + sizeof(std::size_t),((std::size_t*)(v))[0])};
         }
+
 };
 
 
@@ -111,61 +108,27 @@ struct Bytes : public mutils::ByteRepresentable{
 class ByteArrayObject: public mutils::ByteRepresentable {
 public:
   Persistent<Bytes> pers_bytes;
-  //Persistent<Bytes,ST_MEM> vola_bytes;
-
-  //void change_pers_bytes(const Bytes& bytes) {
-  //  *pers_bytes = bytes;
-  //}
 
   void change_pers_bytes(const Bytes& bytes) {
     *pers_bytes = bytes;
   }
 
-  int query_const_int(uint64_t query_us) {
-    return 100;
-  }
-
-  Bytes query_const_bytes(uint64_t query_us) {
-    char bytes[1000000];
-    Bytes b(bytes,1000000);
-    return b;
-  }
-
-  Bytes query_pers_bytes(uint64_t query_us) {
-    HLC hlc{query_us,0};
-    try{
-      return *pers_bytes.get(hlc);
-    } catch (std::exception e){
-      std::cout<<"query_pers_bytes failed:"<<e.what()<<std::endl;
-    }
-    return Bytes();
-  }
-
   /** Named integers that will be used to tag the RPC methods */
 //  enum Functions { CHANGE_PERS_BYTES, CHANGE_VOLA_BYTES };
-  enum Functions { CHANGE_PERS_BYTES, QUERY_PERS_BYTES, QUERY_CONST_INT, QUERY_CONST_BYTES };
+  enum Functions { CHANGE_PERS_BYTES };
 
   static auto register_functions() {
     return std::make_tuple(
-//      derecho::rpc::tag<CHANGE_PERS_BYTES>(&ByteArrayObject::change_pers_bytes));
-      derecho::rpc::tag<QUERY_PERS_BYTES>(&ByteArrayObject::query_pers_bytes),
-      derecho::rpc::tag<QUERY_CONST_INT>(&ByteArrayObject::query_const_int),
-      derecho::rpc::tag<QUERY_CONST_BYTES>(&ByteArrayObject::query_const_bytes),
       derecho::rpc::tag<CHANGE_PERS_BYTES>(&ByteArrayObject::change_pers_bytes));
   }
 
-//  DEFAULT_SERIALIZATION_SUPPORT(ByteArrayObject,pers_bytes,vola_bytes);
   DEFAULT_SERIALIZATION_SUPPORT(ByteArrayObject,pers_bytes);
   // constructor
-//  ByteArrayObject(Persistent<Bytes> & _p_bytes,Persistent<Bytes,ST_MEM> & _v_bytes):
   ByteArrayObject(Persistent<Bytes> & _p_bytes):
-//  ByteArrayObject(Persistent<Bytes> & _p_bytes):
-//    pers_bytes(std::move(_p_bytes)) {
     pers_bytes(std::move(_p_bytes)) {
   }
   // the default constructor
   ByteArrayObject(PersistentRegistry *pr):
-//    pers_bytes(nullptr,pr) {
     pers_bytes(nullptr,pr) {
   }
 };
@@ -176,18 +139,18 @@ int main(int argc, char *argv[]) {
    spdlog::set_level(spdlog::level::trace);  
 #endif
   if(argc != 7) {
-    std::cout<<"usage:"<<argv[0]<<"<shard_size> <num_of_shards> <ops_per_sec> <min_dur_sec> <msg_size> <query_cnt>"<<std::endl;
+    std::cout<<"usage:"<<argv[0]<<" <all|half|one> <num_of_nodes> <msg_size> <count> <max_ops> <query_interval in us>"<<std::endl;
     return -1;
   }
-  int shard_size = atoi(argv[1]);
-  int num_of_shards = atoi(argv[2]);
-  // 1 for shards
-  int num_of_nodes = (shard_size * num_of_shards + 1);
-  int ops_per_sec = atoi(argv[3]);
-  int min_dur_sec = atoi(argv[4]);
-  int msg_size = atoi(argv[5]);
-  int qcnt = atoi(argv[6]);
-  uint64_t si_us = (1000000l/ops_per_sec);
+  int sender_selector = 0; // 0 for all sender
+  if(strcmp(argv[1],"half") == 0)sender_selector=1;
+  if(strcmp(argv[1],"one") == 0)sender_selector=2;
+  int num_of_nodes = atoi(argv[2]);
+  int msg_size = atoi(argv[3]);
+  int count = atoi(argv[4]);
+  int max_ops = atoi(argv[5]);
+  uint64_t si_us = (1000000l/max_ops);
+  // int qi_us = atoi(argv[6]);
 
   derecho::node_id_t node_id;
   derecho::ip_addr my_ip;
@@ -196,31 +159,42 @@ int main(int argc, char *argv[]) {
   long long unsigned int max_msg_size = msg_size;
   long long unsigned int block_size = get_block_size(msg_size);
   derecho::DerechoParams derecho_params{max_msg_size, block_size};
+  bool is_sending = true;
 
   derecho::CallbackSet callback_set{
     nullptr,//we don't need the stability_callback here
+    // the persistence_callback either
     [&](derecho::subgroup_id_t subgroup,derecho::persistence_version_t ver){
+      // TODO: can be used to test the time from send to persistence.
     }
   };
 
   derecho::SubgroupInfo subgroup_info{
-    {{std::type_index(typeid(ByteArrayObject)), [shard_size,num_of_shards,num_of_nodes](const derecho::View& curr_view, int& next_unassigned_rank, bool previous_was_successful) {
+    {{std::type_index(typeid(ByteArrayObject)), [num_of_nodes,sender_selector](const derecho::View& curr_view, int& next_unassigned_rank, bool previous_was_successful) {
       if(curr_view.num_members < num_of_nodes) {
         std::cout << "not enough members yet:"<<curr_view.num_members<<" < "<<num_of_nodes<<std::endl;
         throw derecho::subgroup_provisioning_exception();
       }
       derecho::subgroup_shard_layout_t subgroup_vector(1);
 
-      std::vector<uint32_t> members(shard_size);
-      for(int i=0;i<num_of_shards;i++){
-        for(int j=0;j<shard_size;j++)
-        {
-          members[j] = (uint32_t)i*shard_size + j;
+      std::vector<uint32_t> members(num_of_nodes);
+      std::vector<int> senders(num_of_nodes,1);
+      for(int i=0;i<num_of_nodes;i++){
+        members[i] = i;
+        switch (sender_selector) {
+        case 0: // all senders
+          break;
+        case 1: // half senders
+          if (i <= (num_of_nodes-1)/2)senders[i] = 0;
+          break;
+        case 2: // one senders
+          if (i != (num_of_nodes-1))senders[i] = 0;
+          break;
         }
-        subgroup_vector[0].emplace_back(curr_view.make_subview(members));
       }
 
-      next_unassigned_rank = std::max(next_unassigned_rank, num_of_nodes - 1);
+      subgroup_vector[0].emplace_back(curr_view.make_subview(members,derecho::Mode::ORDERED,senders));
+      next_unassigned_rank = std::max(next_unassigned_rank, num_of_nodes);
       return subgroup_vector;
     }}},
     {std::type_index(typeid(ByteArrayObject))}
@@ -246,11 +220,7 @@ int main(int argc, char *argv[]) {
   bool inadequately_provisioned = true;
   while(inadequately_provisioned) {
     try {
-      if(node_id < (uint32_t)(num_of_nodes - 1)) {
-        group->get_subgroup<ByteArrayObject>();
-      } else {
-        group->get_nonmember_subgroup<ByteArrayObject>();
-      }
+      group->get_subgroup<ByteArrayObject>();
       inadequately_provisioned = false;
     } catch(derecho::subgroup_provisioning_exception& e) {
       inadequately_provisioned = true;
@@ -259,82 +229,135 @@ int main(int argc, char *argv[]) {
 
   std::cout << "All members have joined, subgroups are provisioned." <<std::endl;
 
-  ///////////////////////////////////////////////////////////////////////////////
-  // ordered send.
-  if (node_id < (uint32_t)(num_of_nodes - 1)) {
-    dbg_debug("begin to send message for {} seconds. Message size={}",min_dur_sec,msg_size);
+  uint32_t node_rank = -1;
+  auto members_order = group->get_members();
+  cout << "The order of members is :" << endl;
+  for(uint i = 0; i < (uint32_t)num_of_nodes; ++i) {
+      cout << members_order[i] << " ";
+      if (members_order[i] == node_id) {
+        node_rank = i;
+      }
+  }
+  cout << endl;
+  if((sender_selector == 1) && (node_rank <= (uint32_t)(num_of_nodes-1)/2)) is_sending = false;
+  if((sender_selector == 2) && (node_rank != (uint32_t)num_of_nodes-1)) is_sending = false;
+
+  std::cout << "my rank is:" << node_rank << ", and I'm sending:" << is_sending << std::endl;
+
+  // spawn the query thread
+  derecho::Replicated<ByteArrayObject>& handle = group->get_subgroup<ByteArrayObject>();
+  std::unique_ptr<std::thread> pqt = nullptr;
+  uint64_t *query_time_us; // time used for temporal query
+  uint64_t *appear_time_us; // when the temporal query appears
+  uint64_t *message_time_us; // the latest timestamp
+  int num_datapoints = 0; // number of data points
+  query_time_us = (uint64_t*)malloc(sizeof(uint64_t)*num_of_nodes*count);
+  appear_time_us = (uint64_t*)malloc(sizeof(uint64_t)*num_of_nodes*count);
+  message_time_us = (uint64_t*)malloc(sizeof(uint64_t)*num_of_nodes*count);
+  if(query_time_us == NULL ||
+     appear_time_us == NULL ||
+     message_time_us == NULL) {
+    std::cerr<<"allocate memory error!"<<std::endl;
+  }
+  dbg_debug("about to start the querying thread.");
+#if defined(_PERFORMANCE_DEBUG) || defined(_DEBUG)
+  pqt = std::make_unique<std::thread>([&](){
+      struct timespec tqt;
+      HLC tqhlc;
+      tqhlc.m_logic = 0;
+      uint32_t node_rank_seen = 0xffffffff;
+      uint32_t msg_seqno_seen = 0xffffffff;
+      dbg_debug("query thread is running.");
+      bool bQuit = false;
+      while(!bQuit){
+        clock_gettime(CLOCK_REALTIME,&tqt);
+        tqhlc.m_rtc_us = tqt.tv_sec*1e6 + tqt.tv_nsec/1e3;
+        while(true){
+          try {
+            struct timespec at;
+            std::unique_ptr<Bytes> bs = (*handle.user_object_ptr)->pers_bytes.get(tqhlc);
+            clock_gettime(CLOCK_REALTIME,&at);
+            PayLoad *pl = (PayLoad*)(bs->bytes);
+
+            if(node_rank_seen != pl->node_rank || msg_seqno_seen != pl->msg_seqno){
+              query_time_us[num_datapoints] = tqhlc.m_rtc_us;
+              appear_time_us[num_datapoints] = (uint64_t)(at.tv_sec*1e6+at.tv_nsec/1e3);
+              message_time_us[num_datapoints] = (uint64_t)(pl->tv_sec*1e6+pl->tv_nsec/1e3);
+              num_datapoints++;
+              node_rank_seen = pl->node_rank;
+              msg_seqno_seen = pl->msg_seqno;
+            }
+            
+            dbg_trace("msg_seqno_seen={},count={},num_datapoints={}",msg_seqno_seen,count,num_datapoints);
+            if (msg_seqno_seen == (uint32_t)(count-1)) {
+              std::cout<<"query(us)\tappear(us)\tmessage(us)"<<std::endl;
+              for(int i=0;i<num_datapoints;i++){
+                std::cout<<query_time_us[i]<<"\t"<<appear_time_us[i]<<"\t"<<message_time_us[i]<<std::endl;
+              }
+              dbg_trace("quitting...");
+              bQuit = true;
+            }
+            break;
+          } catch(unsigned long long exp) {
+            dbg_debug("query thread return:{0:x}",exp);
+            // query time is too late or too early:
+            if (exp == PERSIST_EXP_BEYOND_GSF) {
+              usleep(10); // sleep for 10 microseconds.
+            } else if (exp == PERSIST_EXP_INV_HLC) {
+              usleep(10);
+              pthread_yield();
+              dbg_trace("give up query with hlc({},{}) because it is too early.",tqhlc.m_rtc_us,tqhlc.m_logic); 
+              break;
+            } else {
+              dbg_warn("unexpected exception({:x})",exp);
+            }
+          }
+      }
+    }
+    std::cout<<std::flush;
+    exit(0);
+  });
+#endif//_PERFORMANCE_DEBUG || _DEBUG
+  dbg_debug("querying thread started.");
+
+  if(is_sending) {
     char *bbuf = new char[msg_size];
     bzero(bbuf,msg_size);
     Bytes bs(bbuf,msg_size);
 
     try{
 
-      // start - start to send timestamp
-      // last - timestamp of last send
-      // cur - current timestamp
-      struct timespec start,last,cur;
-      int seqno = 0;
+      struct timespec start,cur;
       clock_gettime(CLOCK_REALTIME,&start);
-      cur = start;
-      last = start;
 
 #define DELTA_T_US(t1,t2) ((double)(((t2).tv_sec-(t1).tv_sec)*1e6+((t2).tv_nsec-(t1).tv_nsec)*1e-3))  
 
-      auto& handle = group->get_subgroup<ByteArrayObject>();
-      while (DELTA_T_US(start,cur)/1e6 < min_dur_sec){
+      for(int i=0;i<count;i++) {
           do {
             pthread_yield();
             clock_gettime(CLOCK_REALTIME,&cur);
-          } while ( DELTA_T_US(last,cur) < (double)si_us );
-          
+          } while ( DELTA_T_US(start,cur) < i*(double)si_us );
           {
               ((PayLoad*)bs.bytes)->node_rank = (uint32_t)node_id;
-              ((PayLoad*)bs.bytes)->msg_seqno = (uint32_t)seqno++;
+              ((PayLoad*)bs.bytes)->msg_seqno = (uint32_t)i;
               ((PayLoad*)bs.bytes)->tv_sec    = (uint64_t)cur.tv_sec;
               ((PayLoad*)bs.bytes)->tv_nsec   = (uint64_t)cur.tv_nsec;
               
               handle.ordered_send<ByteArrayObject::CHANGE_PERS_BYTES>(bs);
           }
-          last = cur;
-      };
+      }
 
     } catch (uint64_t exp){
       std::cout<<"Exception caught:0x"<<std::hex<<exp<<std::endl;
       return -1;
     }
   }
-  ///////////////////////////////////////////////////////////////////////////////
-  group->barrier_sync();
-  usleep(min_dur_sec*1e6);
-  // query
-  if (node_id == (uint32_t)(num_of_nodes - 1)) {
-    
-    struct timespec cur,tqs,tqm,tqm1,tqe;
-    clock_gettime(CLOCK_REALTIME,&cur);
-    uint64_t center_ts_us = (cur.tv_sec - min_dur_sec - min_dur_sec/2)*1e6;
-    srandom((unsigned int)center_ts_us);
-    while (qcnt--) {
-      uint64_t query_ts_us = center_ts_us + random()%2000000 - 1000000;
-      clock_gettime(CLOCK_REALTIME,&tqs);
-      auto shard_iterator = group->get_shard_iterator<ByteArrayObject>();
-      // auto query_results_vec = shard_iterator.p2p_query<ByteArrayObject::QUERY_VOLA_BYTES>(query_ts_us);
-      clock_gettime(CLOCK_REALTIME,&tqm1);
-      auto query_results_vec = shard_iterator.p2p_query<ByteArrayObject::QUERY_PERS_BYTES>(query_ts_us);
-      clock_gettime(CLOCK_REALTIME,&tqm);
-      for(auto& query_result:query_results_vec) {
-        auto& reply_map = query_result.get();
-        PayLoad *pl = (PayLoad*)reply_map.begin()->second.get().bytes;
-        volatile uint32_t seq = pl->msg_seqno;
-        seq = seq;
-        // volatile int x = reply_map.begin()->second.get();
-        //dbg_trace("reply from shard {} received. message id = {}",cnt++,pl->msg_seqno);
-      }
-      clock_gettime(CLOCK_REALTIME,&tqe);
-      dbg_trace("get all replies.");
-      std::cout<<"query "<<DELTA_T_US(tqs,tqe)<<" us send "<<DELTA_T_US(tqs,tqm1)<<" "<<DELTA_T_US(tqm1,tqm)<<" us"<<std::endl;
-    }
-  }
-  std::cout<<std::flush;
-  group->barrier_sync();
-  exit(0);
+
+
+#if defined(_PERFORMANCE_DEBUG) || (_DEBUG)
+//      (*handle.user_object_ptr)->vola_bytes.print_performance_stat();
+#endif//_PERFORMANCE_DEBUG
+
+  std::cout << "Reached end of main(), entering infinite loop so program doesn't exit" << std::endl;
+  while(true){}
 }
