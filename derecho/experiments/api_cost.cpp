92225ce8b452747c1e7f77441ed808dc8970ed98
Matthew Milano <milano@cs.cornell.edu>
2018-07-10 17:51:46 -0400

    add couts

--- derecho/experiments/api_cost.cpp
+++ derecho/experiments/api_cost.cpp
@@ -134,10 +134,11 @@ int main(int argc, char** argv) {
     fflush(stdout);
 		
     if(node_id == 0) {
-
-      Replicated<Faz>& faz_rpc_handle = group->get_subgroup<Faz>();
-      cout << "Reading Faz's state just to allow node 1's message to be delivered" << endl;
-      faz_rpc_handle.ordered_query<RPC_NAME(read_state)>();
+        for (auto i = 0u; i < num_messages; ++i){
+        Replicated<Faz>& faz_rpc_handle = group->get_subgroup<Faz>();
+       cout << "Reading Faz's state just to allow node 1's message to be delivered" << endl;
+       faz_rpc_handle.ordered_query<RPC_NAME(read_state)>();
+        }
     }
     if(node_id == 1) {
         Replicated<Faz>& faz_rpc_handle = group->get_subgroup<Faz>();
@@ -146,6 +147,7 @@ int main(int argc, char** argv) {
           cout << "Changing Faz's state round " << i << endl;
           start_times[i] = get_time();
           derecho::rpc::QueryResults<bool> results = faz_rpc_handle.ordered_query<RPC_NAME(change_state)>(new_value);
+          std::cout << "checkpoint: query issued" << std::endl;
           decltype(results)::ReplyMap& replies = results.get();
           cout << "Got a reply map!" << endl;
           for(auto& reply_pair : replies) {
