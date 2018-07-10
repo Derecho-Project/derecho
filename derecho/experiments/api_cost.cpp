d1fc3ffacab555c3f00b217a924789cd194be61b
Matthew Milano <milano@cs.cornell.edu>
2018-07-10 18:04:32 -0400

    sagar is very helpful

--- derecho/experiments/api_cost.cpp
+++ derecho/experiments/api_cost.cpp
@@ -74,7 +74,7 @@ int main(int argc, char** argv) {
       if(sender_id == 0) {
         end_times[index] = get_time();
       }
-      if(index == num_messages - 1 && sender_id == (int)num_nodes - 1) {
+      if(index == num_messages - 1) {
         done = true;
       }
     };
