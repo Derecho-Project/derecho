857ea5a04516218a3106d958df36f9da4f869d0c
Matthew Milano <milano@cs.cornell.edu>
2018-07-12 14:36:48 -0400

    turn the log off

--- derecho/group_impl.h
+++ derecho/group_impl.h
@@ -275,9 +275,7 @@ std::shared_ptr<spdlog::logger> Group<ReplicatedTypes...>::create_logger() const
     log_sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("derecho_debug_log", 1024 * 1024 * 5, 3));
     // Uncomment this to get debugging output printed to the terminal
     log_sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_mt>());
-    std::shared_ptr<spdlog::logger> log = spdlog::create(
-        whendebug("debug_log")
-        whenrelease("release_log"),
+    std::shared_ptr<spdlog::logger> log = spdlog::create("debug_log",
         log_sinks.begin(), log_sinks.end());
     log->set_pattern("[%H:%M:%S.%f] [Thread %t] [%l] %v");
     log->set_level(
