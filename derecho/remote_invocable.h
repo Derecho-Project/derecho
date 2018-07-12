64dc8645a9bb0dc91304312091df56da93638fa6
Matthew Milano <milano@cs.cornell.edu>
2018-07-12 14:07:27 -0400

    whackamole

--- derecho/remote_invocable.h
+++ derecho/remote_invocable.h
@@ -150,7 +150,7 @@ struct RemoteInvoker<Tag, std::function<Ret(Args...)>> {
                                      const node_id_t& nid, const char* response,
                                      const std::function<char*(int)>&) {
         if(response[0]) throw remote_exception_occurred{nid};
-        assert(false && "was not expecting a response!");
+        assert_always(false && "was not expecting a response!");
     }
 
     /**
