bd5df40d764ff3a816380e94a44f62e5496e5572
Matthew Milano <milano@cs.cornell.edu>
2018-07-13 16:44:05 -0400

    sanity check

--- derecho/experiments/api_cost.cpp
+++ derecho/experiments/api_cost.cpp
@@ -128,7 +128,7 @@ int main(int argc, char** argv) {
     //Where do these come from? What do they mean? Does the user really need to supply them?
     long long unsigned int max_msg_size = Faz::test_array_size * sizeof(std::size_t) + 100;
     long long unsigned int block_size = 100000;
-    constexpr auto window_size = 1500u;
+    constexpr auto window_size = 100u;
     derecho::DerechoParams derecho_params{max_msg_size, block_size, window_size};
 
     //note: stability callback isn't going to happen in cooked mode.
