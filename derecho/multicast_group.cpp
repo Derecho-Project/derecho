6b7c10ac97ea0c6b8387a96b55c8f7bdc252b86a
Matthew Milano <milano@cs.cornell.edu>
2018-07-13 15:51:34 -0400

    hack in a middle time

--- derecho/multicast_group.cpp
+++ derecho/multicast_group.cpp
@@ -1282,6 +1282,7 @@ bool MulticastGroup::send(subgroup_id_t subgroup_num) {
     static std::size_t send_count = 0;
     (*middle_times())[send_count] = get_time();
     ++send_count;
+    whendebug(std::cout << "send count is number " << send_count << std::endl);
     if(!rdmc_sst_groups_created) {
         return false;
     }
