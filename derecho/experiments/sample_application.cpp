4184bdeb737b20d7f88e31cedc3341fbc27127ec
Sagar Jha <sjsagarjha3@gmail.com>
2018-10-25 14:20:07 -0400

    fixed a bug in sample_application
    the number of subgroups and the number of shards in each subgroup is a
    constant. It cannot change between views.

--- derecho/experiments/sample_application.cpp
+++ derecho/experiments/sample_application.cpp
@@ -51,7 +51,13 @@ public:
   
 };
 
-int main() {
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        std::cout << "Provide the number of nodes as a command line argument" << std::endl;
+        exit(1);
+    }
+    const uint32_t num_nodes = atoi(argv[1]);
+
     node_id_t my_id;
     ip_addr leader_ip, my_ip;
 
@@ -80,10 +86,10 @@ int main() {
 
     std::map<std::type_index, shard_view_generator_t>
       subgroup_membership_functions{{std::type_index(typeid(TicketBookingSystem)),
-				     [](const View& view, int&, bool) {
+				     [num_nodes](const View& view, int&, bool) {
 				       auto& members = view.members;
 				       auto num_members = members.size();
-				       if (num_members < 3) {
+				       if (num_members < num_nodes) {
 					 throw subgroup_provisioning_exception();
 				       }
 				       subgroup_shard_layout_t layout(num_members);
@@ -108,33 +114,33 @@ int main() {
 
     std::cout << "Finished constructing/joining the group" << std::endl;
     auto group_members = group->get_members();
-    uint32_t my_rank = -1;
+    // uint32_t my_rank = -1;
     std::cout << "Members are" << std::endl;
     for(uint i = 0; i < group_members.size(); ++i) {
         std::cout << group_members[i] << " ";
-        if(group_members[i] == my_id) {
-            my_rank = i;
-        }
+        // if(group_members[i] == my_id) {
+        //   my_rank = i;
+        // }
     }
     std::cout << std::endl;
-    if (my_rank == (uint32_t) -1) {
-        exit(1);
-    }
+    // if (my_rank == (uint32_t) -1) {
+    //     exit(1);
+    // }
 
-    // all members book a ticket
-    Replicated<TicketBookingSystem>& ticketBookingHandle = group->get_subgroup<TicketBookingSystem>();
+    // // all members book a ticket
+    // Replicated<TicketBookingSystem>& ticketBookingHandle = group->get_subgroup<TicketBookingSystem>();
 
-    rpc::QueryResults<bool> results = ticketBookingHandle.ordered_query<RPC_NAME(book)>(my_rank);
-    rpc::QueryResults<bool>::ReplyMap& replies = results.get();
-    for (auto& reply_pair: replies) {
-        std::cout << "Reply from node " << reply_pair.first << ": " << std::boolalpha << reply_pair.second.get() << std::endl;
-    }
+    // rpc::QueryResults<bool> results = ticketBookingHandle.ordered_query<RPC_NAME(book)>(my_rank);
+    // rpc::QueryResults<bool>::ReplyMap& replies = results.get();
+    // for (auto& reply_pair: replies) {
+    //     std::cout << "Reply from node " << reply_pair.first << ": " << std::boolalpha << reply_pair.second.get() << std::endl;
+    // }
 
-    rpc::QueryResults<bool> results2 = ticketBookingHandle.ordered_query<RPC_NAME(book)>(my_rank);
-    rpc::QueryResults<bool>::ReplyMap& replies2 = results2.get();
-    for (auto& reply_pair: replies2) {
-        std::cout << "Reply from node " << reply_pair.first << ": " << std::boolalpha << reply_pair.second.get() << std::endl;
-    }
+    // rpc::QueryResults<bool> results2 = ticketBookingHandle.ordered_query<RPC_NAME(book)>(my_rank);
+    // rpc::QueryResults<bool>::ReplyMap& replies2 = results2.get();
+    // for (auto& reply_pair: replies2) {
+    //     std::cout << "Reply from node " << reply_pair.first << ": " << std::boolalpha << reply_pair.second.get() << std::endl;
+    // }
     
     std::cout << "End of main. Waiting indefinitely" << std::endl;
     while(true) {
