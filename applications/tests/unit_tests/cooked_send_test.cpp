5e9be319f4ae82e9f02755f7972ec8a74397c25e
Sagar Jha <sjsagarjha3@gmail.com>
2018-12-03 19:32:06 -0500

    fixed bugs, removed couts

--- applications/tests/unit_tests/cooked_send_test.cpp
+++ applications/tests/unit_tests/cooked_send_test.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 
 using namespace derecho;
+using std::cout;
+using std::endl;
 using std::map;
 using std::pair;
 using std::vector;
@@ -19,14 +21,12 @@ public:
 
     void send(uint nodeid, uint msg) {
         msgs.push_back(std::make_pair(nodeid, msg));
-        std::cout << "Node " << nodeid << " sent msg " << msg << std::endl;
     }
 
     vector<pair<uint, uint>> get_msgs(uint start_index, uint end_index) {
         uint num_msgs = msgs.size();
         if(end_index > num_msgs) {
             end_index = num_msgs;
-            std::cout << "Msgs size: " << num_msgs << std::endl;
         }
         return vector<pair<uint, uint>>(msgs.begin() + start_index, msgs.begin() + end_index);
     }
@@ -52,12 +52,12 @@ bool verify_local_order(vector<pair<uint, uint>> msgs) {
 int main(int argc, char* argv[]) {
     pthread_setname_np(pthread_self(), "cooked_send");
     if(argc < 2) {
-        std::cout << "Error: Provide the number of nodes as a command line argument!!!" << std::endl;
+        cout << "Error: Provide the number of nodes as a command line argument!!!" << endl;
         exit(1);
     }
     const uint32_t num_nodes = atoi(argv[1]);
     Conf::initialize(argc, argv);
-    std::map<std::type_index, shard_view_generator_t>
+    map<std::type_index, shard_view_generator_t>
             subgroup_membership_functions{
                     {std::type_index(typeid(CookedMessages)),
                      [num_nodes](const View& view, int&) {
@@ -74,37 +74,44 @@ int main(int argc, char* argv[]) {
     auto cooked_subgroup_factory = [](PersistentRegistry*) { return std::make_unique<CookedMessages>(); };
 
     SubgroupInfo subgroup_info(subgroup_membership_functions);
+    volatile bool done = false;
+    uint32_t num_msgs = 500;
+    auto stability_callback = [num_msgs, num_nodes, &done, counter = (uint)0](subgroup_id_t, node_id_t sender_id, message_id_t index, std::optional<std::pair<char*, long long int>>, persistent::version_t) mutable {
+        counter++;
+        if(counter == num_msgs * num_nodes) {
+            done = true;
+        }
+    };
+    Group<CookedMessages> group({stability_callback}, subgroup_info, {}, cooked_subgroup_factory);
 
-    Group<CookedMessages> group({}, subgroup_info, {}, cooked_subgroup_factory);
-
-    std::cout << "Finished constructing/joining the group" << std::endl;
+    cout << "Finished constructing/joining the group" << endl;
     auto group_members = group.get_members();
     uint32_t my_id = getConfUInt32(CONF_DERECHO_LOCAL_ID);
     uint32_t my_rank = -1;
-    std::cout << "Members are" << std::endl;
+    cout << "Members are" << endl;
     for(uint i = 0; i < group_members.size(); ++i) {
-        std::cout << group_members[i] << " ";
+        cout << group_members[i] << " ";
         if(group_members[i] == my_id) {
             my_rank = i;
         }
     }
-    std::cout << std::endl;
+    cout << endl;
     if(my_rank == (uint32_t)-1) {
-        std::cout << "Error: Could not join the group!!!" << std::endl;
+        cout << "Error: Could not join the group!!!" << endl;
         exit(1);
     }
 
     Replicated<CookedMessages>& cookedMessagesHandle = group.get_subgroup<CookedMessages>();
-    uint32_t num_msgs = 500;
     for(uint i = 1; i < num_msgs + 1; ++i) {
         cookedMessagesHandle.ordered_send<RPC_NAME(send)>(my_rank, i);
     }
-
+    while(!done) {
+    }
     if(my_rank == 0) {
         uint32_t max_msg_size = getConfUInt64(CONF_DERECHO_MAX_PAYLOAD_SIZE);
-        uint32_t num_entries = max_msg_size / sizeof(pair<uint, uint>) - 5;
+        uint32_t num_entries = max_msg_size / sizeof(pair<uint, uint>) - 50;
         if(num_entries < 0) {
-            std::cout << "Error: Maximum message size too small!" << std::endl;
+            cout << "Error: Maximum message size too small!" << endl;
             exit(1);
         }
         map<node_id_t, vector<pair<uint, uint>>> msgs_map;
@@ -118,18 +125,18 @@ int main(int argc, char* argv[]) {
         }
         vector<pair<uint, uint>> first_reply = msgs_map.begin()->second;
         if(!verify_local_order(first_reply)) {
-            std::cout << "Error: Violation of local order!!!" << std::endl;
+            cout << "Error: Violation of local order!!!" << endl;
             exit(1);
         }
-        std::cout << "Local ordering test successful!" << std::endl;
+        cout << "Local ordering test successful!" << endl;
         for(auto& msgs_map_entry : msgs_map) {
             vector<pair<uint, uint>>& v = msgs_map_entry.second;
             if(first_reply != v) {
-                std::cout << "Error: Violation of global order!!!" << std::endl;
+                cout << "Error: Violation of global order!!!" << endl;
                 exit(1);
             }
         }
-        std::cout << "Global ordering test successful!" << std::endl;
+        cout << "Global ordering test successful!" << endl;
     }
 
     group.barrier_sync();
