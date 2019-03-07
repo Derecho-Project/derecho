11c80598fc64437b2c7c892a4cdeef4b2bbc5ac7
Weijia Song <songweijia@gmail.com>
2019-03-07 15:37:17 -0500

    minor bug fix

--- objectstore/performance.cpp
+++ objectstore/performance.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv) {
     }
 
     bool use_aio = false;
-    if(strcmp("aio", argv[1]) != 0) {
+    if(strcmp("aio", argv[argc - NUM_APP_ARGS]) != 0) {
         use_aio = true;
     } else if ( strcmp("bio", argv[1]) != 0 ) {
         std::cerr << "unrecognized argument:" << argv[argc - NUM_APP_ARGS] << ". Using bio (blocking io) instead." <<
