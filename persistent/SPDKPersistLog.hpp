3a46684764b7588b5a6b8a83053b6c30acb9c5c1
Weijia Song <songweijia@gmail.com>
2019-03-18 15:55:52 -0400

    framework for SPDK porting.

--- persistent/SPDKPersistLog.hpp
+++ persistent/SPDKPersistLog.hpp
@@ -1,75 +1,25 @@
-#ifndef PERSIST_LOG_HPP
-#define PERSIST_LOG_HPP
+#ifndef SPDK_PERSIST_LOG_HPP
+#define SPDK_PERSIST_LOG_HPP
 
 #if !defined(__GNUG__) && !defined(__clang__)
-#error PersistLog.hpp only works with clang and gnu compilers
+#error SPDKPersistLog.hpp only works with clang and gnu compilers
 #endif
 
-#include "../HLC.hpp"
-#include "../PersistException.hpp"
-#include "../PersistentTypenames.hpp"
-#include <functional>
-#include <inttypes.h>
-#include <map>
-#include <set>
-#include <stdio.h>
-#include <string>
+#include "PersistLog.hpp"
 
 namespace persistent {
 
-// Storage type:
-enum StorageType {
-    ST_FILE = 0,
-    ST_MEM,
-    ST_SPDK
-};
-
-//#define INVALID_VERSION ((__int128)-1L)
-#define INVALID_VERSION ((int64_t)-1L)
-#define INVALID_INDEX INT64_MAX
-
-// index entry for the hlc index
-struct hlc_index_entry {
-    HLC hlc;
-    int64_t log_idx;
-    //default constructor
-    hlc_index_entry() : log_idx(-1) {
-    }
-    // constructor with hlc and index.
-    hlc_index_entry(const HLC &_hlc, const int64_t &_log_idx) : hlc(_hlc), log_idx(_log_idx) {
-    }
-    // constructor with time stamp and index.
-    hlc_index_entry(const uint64_t &r, const uint64_t &l, const int64_t &_log_idx) : hlc(r, l), log_idx(_log_idx) {
-    }
-    //copy constructor
-    hlc_index_entry(const struct hlc_index_entry &_entry) : hlc(_entry.hlc), log_idx(_entry.log_idx) {
-    }
-};
-// comparator for the hlc index
-struct hlc_index_entry_comp {
-    bool operator()(const struct hlc_index_entry &e1,
-                    const struct hlc_index_entry &e2) const {
-        return e1.hlc < e2.hlc;
-    }
-};
-
 // Persistent log interfaces
-class PersistLog {
+class SPDKPersistLog : public PersistLog {
 public:
-    // LogName
-    const std::string m_sName;
-    // HLCIndex
-    std::set<hlc_index_entry, hlc_index_entry_comp> hidx;
-#ifndef NDEBUG
-    void dump_hidx();
-#endif  //NDEBUG
     // Constructor:
     // Remark: the constructor will check the persistent storage
     // to make sure if this named log(by "name" in the template
     // parameters) is already there. If it is, load it from disk.
     // Otherwise, create the log.
-    PersistLog(const std::string &name) noexcept(true);
-    virtual ~PersistLog() noexcept(true);
+    SPDKPersistLog(const std::string &name) : PersistLog(name) noexcept(true) {}
+    // Destructor
+    virtual ~SPDKPersistLog() noexcept(true);
     /** Persistent Append
      * @param pdata - serialized data to be append
      * @param size - length of the data
@@ -82,50 +32,46 @@ public:
      */
     virtual void append(const void *pdata,
                         const uint64_t &size, const version_t &ver,  
-                        const HLC &mhlc) noexcept(false)
-            = 0;
+                        const HLC &mhlc) noexcept(false);
 
     /**
      * Advance the version number without appendding a log. This is useful
      * to create gap between versions.
      */
     // virtual void advanceVersion(const __int128 & ver) noexcept(false) = 0;
-    virtual void advanceVersion(const version_t &ver) noexcept(false) = 0;
+    virtual void advanceVersion(const version_t &ver) noexcept(false);
 
     // Get the length of the log
-    virtual int64_t getLength() noexcept(false) = 0;
+    virtual int64_t getLength() noexcept(false);
 
     // Get the Earliest Index
-    virtual int64_t getEarliestIndex() noexcept(false) = 0;
+    virtual int64_t getEarliestIndex() noexcept(false);
 
     // Get the Latest Index
-    virtual int64_t getLatestIndex() noexcept(false) = 0;
+    virtual int64_t getLatestIndex() noexcept(false);
 
     // Get the Index corresponding to a version
-    virtual int64_t getVersionIndex(const version_t &ver) noexcept(false) = 0;
-
-    // Get the Index corresponding to an HLC timestamp
-    virtual int64_t getHLCIndex(const HLC& hlc) noexcept(false) = 0;
+    virtual int64_t getVersionIndex(const version_t &ver);
 
     // Get the Earlist version
-    virtual version_t getEarliestVersion() noexcept(false) = 0;
+    virtual version_t getEarliestVersion() noexcept(false);
 
     // Get the Latest version
-    virtual version_t getLatestVersion() noexcept(false) = 0;
+    virtual version_t getLatestVersion() noexcept(false);
 
     // return the last persisted value
-    virtual const version_t getLastPersisted() noexcept(false) = 0;
+    virtual const version_t getLastPersisted() noexcept(false);
 
     // Get a version by entry number return both length and buffer
-    virtual const void *getEntryByIndex(const int64_t &eno) noexcept(false) = 0;
+    virtual const void *getEntryByIndex(const int64_t &eno) noexcept(false);
 
     // Get the latest version equal or earlier than ver.
-    virtual const void *getEntry(const version_t &ver) noexcept(false) = 0;
+    virtual const void *getEntry(const version_t &ver) noexcept(false);
 
     // Get the latest version - deprecated.
     // virtual const void* getEntry() noexcept(false) = 0;
     // Get a version specified by hlc
-    virtual const void *getEntry(const HLC &hlc) noexcept(false) = 0;
+    virtual const void *getEntry(const HLC &hlc) noexcept(false);
 
     /**
      * Persist the log till specified version
@@ -133,33 +79,33 @@ public:
      * Note that the return value could be higher than the the version asked
      * is lower than the log that has been actually persisted.
      */
-    virtual const version_t persist(const bool preLocked = false) noexcept(false) = 0;
+    virtual const version_t persist(const bool preLocked = false) noexcept(false);
 
     /**
      * Trim the log till entry number eno, inclusively.
      * For exmaple, there is a log: [7,8,9,4,5,6]. After trim(3), it becomes [5,6]
      * @param eno -  the log number to be trimmed
      */
-    virtual void trimByIndex(const int64_t &idx) noexcept(false) = 0;
+    virtual void trimByIndex(const int64_t &idx) noexcept(false);
 
     /**
      * Trim the log till version, inclusively.
      * @param ver - all log entry before ver will be trimmed.
      */
-    virtual void trim(const version_t &ver) noexcept(false) = 0;
+    virtual void trim(const version_t &ver) noexcept(false);
 
     /**
      * Trim the log till HLC clock, inclusively.
      * @param hlc - all log entry before hlc will be trimmed.
      */
-    virtual void trim(const HLC &hlc) noexcept(false) = 0;
+    virtual void trim(const HLC &hlc) noexcept(false);
 
     /**
      * Calculate the byte size required for serialization
      * @PARAM ver - from which version the detal begins(tail log) 
      *   INVALID_VERSION means to include all of the tail logs
      */
-    virtual size_t bytes_size(const version_t &ver) = 0;
+    virtual size_t bytes_size(const version_t &ver);
 
     /**
      * Write the serialized log bytes to the given buffer
@@ -167,7 +113,7 @@ public:
      * @PARAM ver - from which version the detal begins(tail log)
      *   INVALID_VERSION means to include all of the tail logs
      */
-    virtual size_t to_bytes(char *buf, const version_t &ver) = 0;
+    virtual size_t to_bytes(char *buf, const version_t &ver);
 
     /**
      * Post the serialized log bytes to a function
@@ -176,22 +122,21 @@ public:
      *   INVALID_VERSION means to include all of the tail logs
      */
     virtual void post_object(const std::function<void(char const *const, std::size_t)> &f,
-                             const version_t &ver)
-            = 0;
+                             const version_t &ver);
 
     /**
      * Check/Merge the LogTail to the existing log.
      * @PARAM dsm - deserialization manager
      * @PARAM v - serialized log bytes to be apllied
      */
-    virtual void applyLogTail(char const *v) = 0;
+    virtual void applyLogTail(char const *v);
 
     /**
      * Truncate the log strictly newer than 'ver'.
      * @param ver - all log entry strict after ver will be truncated.
      */
-    virtual void truncate(const version_t &ver) noexcept(false) = 0;
+    virtual void truncate(const version_t &ver) noexcept(false);
 };
 }
 
-#endif  //PERSIST_LOG_HPP
+#endif  //SPDK_PERSIST_LOG_HPP
