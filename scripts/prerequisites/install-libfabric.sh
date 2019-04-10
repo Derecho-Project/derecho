0fc21bc50b0c16f7eb0642a940f2a773805b91c8
Weijia <songweijia@gmail.com>
2019-04-10 02:16:49 -0400

    installation script:run libtoolize before installing libfabric.

--- scripts/prerequisites/install-libfabric.sh
+++ scripts/prerequisites/install-libfabric.sh
@@ -9,6 +9,7 @@ echo "Using INSTALL_PREFIX=${INSTALL_PREFIX}"
 git clone https://github.com/ofiwg/libfabric.git
 cd libfabric
 git checkout fcf0f2ec3c7109e06e09d3650564df8d2dfa12b6 
+libtoolize
 ./autogen.sh
 ./configure --prefix=${INSTALL_PREFIX} --disable-mlx
 make -j `lscpu | grep "^CPU(" | awk '{print $2}'`
