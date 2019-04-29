818a2e52ceaab6330aebc1130dd394ca44cfda61
Edward Tremel <edward@cs.cornell.edu>
2019-04-29 11:22:33 -0400

    Changed default subgroup allocator to not reassign nodes
    It turns out that reassigning a node from one subgroup or shard to
    another is not well supported by the rest of Derecho. The default
    subgroup allocation function now ensures that nodes are never reassigned
    to a different role, even if that means marking a view "inadequate"
    unnecessarily (i.e. the view could become adequate by reassigning a node
    from a shard with extra replicas to a shard with insufficient replicas).

--- include/derecho/core/subgroup_functions.hpp
+++ include/derecho/core/subgroup_functions.hpp
@@ -9,8 +9,8 @@
 #include <memory>
 #include <variant>
 
-#include "detail/derecho_internal.hpp"
 #include "derecho_modes.hpp"
+#include "detail/derecho_internal.hpp"
 #include "subgroup_info.hpp"
 
 namespace derecho {
@@ -205,12 +205,17 @@ protected:
      * if the View doesn't have enough members for even the minimum-size
      * allocation.
      * @param subgroup_type_order The same subgroup type order passed in to the operator() function
+     * @param prev_view The previous View, if there is one. This is used to
+     * ensure that every shard can keep the non-failed nodes it had in the
+     * previous View, even if the even-incrementing rule would assign it fewer
+     * nodes in this View.
      * @param curr_view The current View
      * @return A map from subgroup type -> subgroup index -> shard number
      * -> number of nodes in that shard
      */
     std::map<std::type_index, std::vector<std::vector<uint32_t>>> compute_standard_shard_sizes(
             const std::vector<std::type_index>& subgroup_type_order,
+            const std::unique_ptr<View>& prev_view,
             const View& curr_view) const;
 
     /**
@@ -227,25 +232,6 @@ protected:
             const std::map<std::type_index, std::vector<std::vector<uint32_t>>>& shard_sizes) const;
 
     /**
-     * Determines which nodes in the previous View's subgroup allocation will
-     * need to get reassigned to new subgroups in the current View because the
-     * shard-size function has determined that their shard must shrink. (Nodes
-     * from the previous View that have failed in the current View are not
-     * counted). This is a necessary first pass before the actual new assignments
-     * can be computed.
-     * @param subgroup_type_order The same subgroup type order passed in to the operator() function
-     * @param prev_view The previous View (which is not null)
-     * @param curr_view The current View
-     * @param shard_sizes The map of membership sizes for every subgroup and shard in curr_view
-     * @return A list of node IDs of nodes that will be leaving their prev_view
-     * shard for a different shard in curr_view
-     */
-    std::list<node_id_t> find_reassigned_nodes(
-            const std::vector<std::type_index>& subgroup_type_order,
-            const std::unique_ptr<View>& prev_view, const View& curr_view,
-            const std::map<std::type_index, std::vector<std::vector<uint32_t>>>& shard_sizes) const;
-
-    /**
      * Creates and returns a new membership allocation for a single subgroup
      * type, based on its previous allocation and its newly-assigned sizes.
      * @param subgroup_type The subgroup type to allocate members for
@@ -254,10 +240,6 @@ protected:
      * @param prev_view The previous View, now known to be non-null
      * @param curr_view The current View, whose next_unassigned_rank will be updated
      * @param shard_sizes The map of membership sizes for every subgroup and shard in curr_view
-     * @param free_reassigned_nodes The list of node IDs that can be reassigned
-     * to shards that need more members (even though they are before next_unassigned_rank
-     * in the members list). Each invocation of this function will modify this parameter
-     * to remove any nodes it uses in its subgroup's allocation.
      * @return A subgroup layout for this subgroup type.
      */
     subgroup_shard_layout_t update_standard_subgroup_type(
@@ -265,8 +247,7 @@ protected:
             const subgroup_type_id_t subgroup_type_id,
             const std::unique_ptr<View>& prev_view,
             View& curr_view,
-            const std::map<std::type_index, std::vector<std::vector<uint32_t>>>& shard_sizes,
-            std::list<node_id_t>& free_reassigned_nodes) const;
+            const std::map<std::type_index, std::vector<std::vector<uint32_t>>>& shard_sizes) const;
 
     /**
      * Helper function that implements the subgroup allocation algorithm for all
