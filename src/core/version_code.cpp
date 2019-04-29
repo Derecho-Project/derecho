093035534e560d5fb27d60cd1145963ace244f22
Sagar Jha <sjsagarjha3@gmail.com>
2019-04-29 18:45:18 -0400

    Just use offsetof

--- src/core/version_code.cpp
+++ src/core/version_code.cpp
@@ -41,34 +41,27 @@ struct s1 {
     char something;
     int16_t the_int;
 };
-
-const uint64_t int16_offset = reinterpret_cast<const uint64_t>(
-        &reinterpret_cast<struct s1*>(dnull_ptr)->the_int);
+const uint64_t int16_offset = (uint64_t)offsetof(struct s1, the_int);
 
 struct s2 {
     char something;
     int32_t the_int;
 };
-
-const uint64_t int32_offset = reinterpret_cast<const uint64_t>(
-        &reinterpret_cast<struct s2*>(dnull_ptr)->the_int);
+const uint64_t int32_offset = (uint64_t)offsetof(struct s2, the_int);
 
 struct s3 {
     char something;
     int64_t the_int;
 };
 
-const uint64_t int64_offset1 = reinterpret_cast<const uint64_t>(
-        &reinterpret_cast<struct s3*>(dnull_ptr)->the_int);
+const uint64_t int64_offset1 = (uint64_t)offsetof(struct s3, the_int);
 
 struct s4 {
     char something;
     float fsomething;
     int64_t the_int;
 };
-
-const uint64_t int64_offset2 = reinterpret_cast<const uint64_t>(
-        &reinterpret_cast<struct s4*>(dnull_ptr)->the_int);
+const uint64_t int64_offset2 = (uint64_t)offsetof(struct s4, the_int);
 
 struct s5 {
     char something;
@@ -76,8 +69,7 @@ struct s5 {
     int64_t the_int;
 };
 
-const uint64_t int64_offset3 = reinterpret_cast<const uint64_t>(
-        &reinterpret_cast<struct s5*>(dnull_ptr)->the_int);
+const uint64_t int64_offset3 = (uint64_t)offsetof(struct s5, the_int);
 
 uint64_t int_offset_hash = mix(int64_offset3,
                                mix(int64_offset2,
