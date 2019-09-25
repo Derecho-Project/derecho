21645151969ec247f0c8a94f25d016b53aaa80d1
Weijia Song <songweijia@gmail.com>
2019-09-25 15:58:44 -0400

    updated scripts to use latest mutils library

--- scripts/prerequisites/install-mutils.sh
+++ scripts/prerequisites/install-mutils.sh
@@ -8,7 +8,7 @@ echo "Using INSTALL_PREFIX=${INSTALL_PREFIX}"
 
 git clone https://github.com/mpmilano/mutils.git
 cd mutils
-git checkout 7072b25e433148006dfca1dc100eb45050b2db90
+git checkout f1bdc9f2224a6a85d91acd2703e346c52f0f1a42
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ..
