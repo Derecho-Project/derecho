91ceeeb31833d86a1e0af336778a859aa61a49cb
Weijia Song <songweijia@gmail.com>
2019-10-19 17:08:09 -0400

    updated install-mutils*.sh scripts

--- scripts/prerequisites/install-mutils.sh
+++ scripts/prerequisites/install-mutils.sh
@@ -12,7 +12,8 @@ WORKPATH=`mktemp -d`
 cd ${WORKPATH}
 git clone https://github.com/mpmilano/mutils.git
 cd mutils
-git checkout f1bdc9f2224a6a85d91acd2703e346c52f0f1a42
+# git checkout f1bdc9f2224a6a85d91acd2703e346c52f0f1a42
+git checkout 2b6f80b60f2b1695bd27dce8dd30cbc3170d7290
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ..
