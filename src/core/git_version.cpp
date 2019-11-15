d36c56d4f4317d582568813a0ec208e0dc72a880
Edward Tremel <edward@cs.cornell.edu>
2019-11-15 16:53:17 -0500

    Changed version generation to use a git hook. Fixes #147
    This will only work if all clients use the provided pre-commit hook,
    otherwise the committed file git_versions.cpp will get out of date.
    Unfortunately, there's no way to force client repositories to use a
    specific pre-commit hook, no matter how important it is.

---
+++ src/core/git_version.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file git_version.cpp
+ *
+ * This file is automatically created by the pre-commit hook that is distributed 
+ * with Derecho in the githooks folder; this script should be installed in your 
+ * local repository's .git/hooks/ directory.
+ */
+
+#include <derecho/core/git_version.hpp>
+
+namespace derecho {
+
+const int MAJOR_VERSION = 0;
+const int MINOR_VERSION = 9;
+const int PATCH_VERSION = 2;
+const int COMMITS_AHEAD_OF_VERSION = 19;
+const char* VERSION_STRING = "0.9.2";
+const char* VERSION_STRING_PLUS_COMMITS = "0.9.2+19";
+
+}
