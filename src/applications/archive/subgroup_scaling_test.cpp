e6a6f3cf733a6b96a1c8267f805b07a30c84f23f
ellerre <lorenzo.rosa.bo@gmail.com>
2020-01-14 18:53:15 -0500

    Code aligne with network_test branch

--- src/applications/archive/subgroup_scaling_test.cpp
+++ src/applications/archive/subgroup_scaling_test.cpp
@@ -35,12 +35,14 @@ struct volatile_wrapper {
 struct exp_result {
     uint32_t num_nodes;
     long long unsigned int max_msg_size;
+    uint32_t num_subgroups;
     uint32_t subgroup_size;
     double bw;
 
     void print(std::ofstream &fout) {
         fout << num_nodes << " "
              << max_msg_size << " "
+             << num_subgroups << " "
              << subgroup_size << " "
              << bw << endl;
     }
@@ -48,36 +50,46 @@ struct exp_result {
 
 int main(int argc, char *argv[]) {
     try {
-        if(argc < 3) {
-            cout << "Insufficient number of command line arguments" << endl;
-            cout << "Enter num_nodes, subgroup_size" << endl;
+        if(argc != 6) {
+            cout << "Invalid command line arguments." << endl;
+            cout << "USAGE:" << argv[0] << " num_nodes, num_groups, subgroup_size, num_messages, msg_size" << endl;
             cout << "Thank you" << endl;
             return -1;
         }
         pthread_setname_np(pthread_self(), "sbgrp_scaling");
 
         const uint32_t num_nodes = std::stoi(argv[1]);
+        const uint32_t num_subgroups = std::stoi(argv[2]);
+        const uint32_t subgroup_size = std::stoi(argv[3]);
+        const uint32_t num_messages = std::stoi(argv[4]);
+        const uint64_t max_msg_size = std::stoi(argv[5]);
 
+        if(subgroup_size > num_nodes) {
+            cout << "Subgroup size must be at most num nodes!" << endl;
+            cout << "USAGE:" << argv[0] << " num_nodes, subgroup_size, num_messages, msg_size" << endl;
+            cout << "Thank you" << endl;
+            return -1;
+        }
+
+        // Read configurations from the command line options as well as the default config file
         Conf::initialize(argc, argv);
+        const uint32_t node_id = getConfUInt32(CONF_DERECHO_LOCAL_ID);
 
-        uint64_t max_msg_size = getConfUInt64(CONF_SUBGROUP_DEFAULT_MAX_PAYLOAD_SIZE);
-        uint32_t num_messages = ((max_msg_size < 20000) ? 10000 : 1000);
-
-        uint32_t node_id = getConfUInt32(CONF_DERECHO_LOCAL_ID);
-
-        // will resize it as and when convenient
-        uint32_t subgroup_size = std::stoi(argv[2]);
-        const auto num_subgroups = num_nodes;
+       /* Create subgroups. 
+        * Subgroup creation is based on the node index.
+        * 
+        * 
+        */ 
         vector<uint32_t> send_subgroup_indices;
         map<uint32_t, uint32_t> subgroup_to_local_index;
         for(uint i = 0; i < subgroup_size; ++i) {
             auto j = ((int32_t)(node_id + num_nodes) - (int32_t)i) % num_nodes;
-            subgroup_to_local_index[j] = i;
             if(j < num_subgroups) {
+		subgroup_to_local_index[j] = i;
                 send_subgroup_indices.push_back(j);
             }
         }
-        vector<vector<volatile_wrapper<long long int>>> received_message_indices(subgroup_size);
+	vector<vector<volatile_wrapper<long long int>>> received_message_indices(subgroup_size);
         for(uint i = 0; i < subgroup_size; ++i) {
             received_message_indices[i].resize(subgroup_size, -1);
         }
@@ -89,7 +101,7 @@ int main(int argc, char *argv[]) {
             received_message_indices[subgroup_to_local_index.at(subgroup_num)][sender_rank] = index;
         };
 
-        auto membership_function = [num_nodes, subgroup_size](
+        auto membership_function = [num_subgroups, num_nodes, subgroup_size](
                 const std::vector<std::type_index>& subgroup_type_order,
                 const std::unique_ptr<derecho::View>& prev_view, derecho::View& curr_view) {
             derecho::subgroup_allocation_map_t subgroup_allocation;
@@ -97,8 +109,8 @@ int main(int argc, char *argv[]) {
             if(num_members < num_nodes) {
                 throw subgroup_provisioning_exception();
             }
-            subgroup_shard_layout_t subgroup_vector(num_members);
-            for(uint i = 0; i < num_members; ++i) {
+            subgroup_shard_layout_t subgroup_vector(num_subgroups);
+            for(uint i = 0; i < num_subgroups; ++i) {
                 vector<uint32_t> members(subgroup_size);
                 for(uint j = 0; j < subgroup_size; ++j) {
                     members[j] = (i + j) % num_members;
@@ -108,7 +120,7 @@ int main(int argc, char *argv[]) {
             curr_view.next_unassigned_rank = curr_view.members.size();
             subgroup_allocation.emplace(std::type_index(typeid(RawObject)), std::move(subgroup_vector));
             return subgroup_allocation;
-        };
+	};
 
         SubgroupInfo raw_groups(membership_function);
 
@@ -122,10 +134,10 @@ int main(int argc, char *argv[]) {
         }
         auto members_order = managed_group.get_members();
         auto node_rank = managed_group.get_my_rank();
-	
+
         vector<std::reference_wrapper<Replicated<RawObject>>> subgroups;
         for(uint i = 0; i < subgroup_size; ++i) {
-            subgroups.emplace_back(managed_group.get_subgroup<RawObject>((node_id - i + num_nodes) % num_nodes));
+            subgroups.emplace_back(managed_group.get_subgroup<RawObject>((node_id - i + num_subgroups) % num_subgroups));
         }
 
         auto send_some = [&](uint32_t num_subgroups) {
@@ -145,7 +157,7 @@ int main(int argc, char *argv[]) {
                 auto i = p.second;
                 for(uint j = 0; j < subgroup_size; ++j) {
                     if(received_message_indices[i][j] < num_messages - 1) {
-                        return false;
+			return false;
                     }
                 }
             }
@@ -165,7 +177,7 @@ int main(int argc, char *argv[]) {
         bw = (max_msg_size * num_messages * num_nodes * send_subgroup_indices.size() + 0.0) / nanoseconds_elapsed;
         avg_bw = aggregate_bandwidth(members_order, node_id, bw);
         if(node_rank == 0) {
-            log_results(exp_result{num_nodes, max_msg_size, subgroup_size, avg_bw},
+            log_results(exp_result{num_nodes, max_msg_size, num_subgroups, subgroup_size, avg_bw},
                         "data_subgroup_scaling");
         }
 
