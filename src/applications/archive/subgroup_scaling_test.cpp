8690cbd593e54bbda59e0e319b889a2137f7acae
ellerre <lorenzo.rosa.bo@gmail.com>
2020-01-14 17:01:33 -0500

    Extended subgroup_scaling_test to allow a number of subgroups different from the number of nodes

--- src/applications/archive/subgroup_scaling_test.cpp
+++ src/applications/archive/subgroup_scaling_test.cpp
@@ -48,18 +48,23 @@ struct exp_result {
 
 int main(int argc, char *argv[]) {
     try {
-        if(argc != 5) {
+        if(argc != 6) {
             cout << "Invalid command line arguments." << endl;
+<<<<<<< HEAD
+            cout << "USAGE:" << argv[0] << " num_nodes, num_groups, subgroup_size, num_messages, msg_size" << endl;
+=======
             cout << "USAGE:" << argv[0] << " num_nodes, subgroup_size, num_messages, msg_size" << endl;
+>>>>>>> 239cee56b17780783c5ed7c75a5f649c49e9e84c
             cout << "Thank you" << endl;
             return -1;
         }
         pthread_setname_np(pthread_self(), "sbgrp_scaling");
 
         const uint32_t num_nodes = std::stoi(argv[1]);
-        const uint32_t subgroup_size = std::stoi(argv[2]);
-        const uint32_t num_messages = std::stoi(argv[3]);
-        const uint64_t max_msg_size = std::stoi(argv[4]);
+        const uint32_t num_subgroups = std::stoi(argv[2]);
+        const uint32_t subgroup_size = std::stoi(argv[3]);
+        const uint32_t num_messages = std::stoi(argv[4]);
+        const uint64_t max_msg_size = std::stoi(argv[5]);
 
         if(subgroup_size > num_nodes) {
             cout << "Subgroup size must be at most num nodes!" << endl;
@@ -77,17 +82,16 @@ int main(int argc, char *argv[]) {
         * 
         * 
         */ 
-        const auto num_subgroups = num_nodes;
         vector<uint32_t> send_subgroup_indices;
         map<uint32_t, uint32_t> subgroup_to_local_index;
         for(uint i = 0; i < subgroup_size; ++i) {
             auto j = ((int32_t)(node_id + num_nodes) - (int32_t)i) % num_nodes;
-            subgroup_to_local_index[j] = i;
             if(j < num_subgroups) {
+		subgroup_to_local_index[j] = i;
                 send_subgroup_indices.push_back(j);
             }
         }
-        vector<vector<volatile_wrapper<long long int>>> received_message_indices(subgroup_size);
+	vector<vector<volatile_wrapper<long long int>>> received_message_indices(subgroup_size);
         for(uint i = 0; i < subgroup_size; ++i) {
             received_message_indices[i].resize(subgroup_size, -1);
         }
@@ -96,10 +100,12 @@ int main(int argc, char *argv[]) {
                                    &received_message_indices](uint32_t subgroup_num, int sender_id, long long int index,
                                                               std::optional<std::pair<char*, long long int>> data, persistent::version_t ver) mutable {
             int sender_rank = (sender_id - subgroup_num + num_nodes) % num_nodes;
-            received_message_indices[subgroup_to_local_index.at(subgroup_num)][sender_rank] = index;
+//	    if(subgroup_num < num_subgroups) {
+            	received_message_indices[subgroup_to_local_index.at(subgroup_num)][sender_rank] = index;
+//	    }
         };
 
-        auto membership_function = [num_nodes, subgroup_size](
+        auto membership_function = [num_subgroups, num_nodes, subgroup_size](
                 const std::vector<std::type_index>& subgroup_type_order,
                 const std::unique_ptr<derecho::View>& prev_view, derecho::View& curr_view) {
             derecho::subgroup_allocation_map_t subgroup_allocation;
@@ -107,8 +113,8 @@ int main(int argc, char *argv[]) {
             if(num_members < num_nodes) {
                 throw subgroup_provisioning_exception();
             }
-            subgroup_shard_layout_t subgroup_vector(num_members);
-            for(uint i = 0; i < num_members; ++i) {
+            subgroup_shard_layout_t subgroup_vector(num_subgroups);
+            for(uint i = 0; i < num_subgroups; ++i) {
                 vector<uint32_t> members(subgroup_size);
                 for(uint j = 0; j < subgroup_size; ++j) {
                     members[j] = (i + j) % num_members;
@@ -118,7 +124,7 @@ int main(int argc, char *argv[]) {
             curr_view.next_unassigned_rank = curr_view.members.size();
             subgroup_allocation.emplace(std::type_index(typeid(RawObject)), std::move(subgroup_vector));
             return subgroup_allocation;
-        };
+	};
 
         SubgroupInfo raw_groups(membership_function);
 
@@ -132,10 +138,10 @@ int main(int argc, char *argv[]) {
         }
         auto members_order = managed_group.get_members();
         auto node_rank = managed_group.get_my_rank();
-	
+
         vector<std::reference_wrapper<Replicated<RawObject>>> subgroups;
         for(uint i = 0; i < subgroup_size; ++i) {
-            subgroups.emplace_back(managed_group.get_subgroup<RawObject>((node_id - i + num_nodes) % num_nodes));
+            subgroups.emplace_back(managed_group.get_subgroup<RawObject>((node_id - i + num_subgroups) % num_subgroups));
         }
 
         auto send_some = [&](uint32_t num_subgroups) {
@@ -155,7 +161,7 @@ int main(int argc, char *argv[]) {
                 auto i = p.second;
                 for(uint j = 0; j < subgroup_size; ++j) {
                     if(received_message_indices[i][j] < num_messages - 1) {
-                        return false;
+			return false;
                     }
                 }
             }
