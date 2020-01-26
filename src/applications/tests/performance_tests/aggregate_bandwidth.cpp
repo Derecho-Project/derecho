5ef15c28bc3a9a6d5a483e95034fd1db535067df
Sagar Jha <sjsagarjha3@gmail.com>
2020-01-26 15:23:33 -0500

    Alternate way to aggregate bandwidth to work around the lf issue

--- src/applications/tests/performance_tests/aggregate_bandwidth.cpp
+++ src/applications/tests/performance_tests/aggregate_bandwidth.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 
+#include <derecho/tcp/tcp.hpp>
+
 #include "aggregate_bandwidth.hpp"
 
 double aggregate_bandwidth(std::vector<uint32_t> members, uint32_t node_id,
@@ -14,4 +16,25 @@ double aggregate_bandwidth(std::vector<uint32_t> members, uint32_t node_id,
         total_bw += sst.bw[i];
     }
     return total_bw / num_nodes;
+}
+
+double aggregate_bandwidth_tcp(std::string leader_ip, bool is_leader, uint32_t num_members,
+                               double bw) {
+    uint16_t port = 9826;
+    double total_bw = bw;
+    if (is_leader) {
+        tcp::connection_listener c_l(port);
+        for(uint i = 0; i < num_members - 1; ++i) {
+            tcp::socket s = c_l.accept();
+	    double recvd_bw;
+	    s.read(recvd_bw);
+	    total_bw += recvd_bw;
+        }
+    }
+    else {
+        tcp::socket s(leader_ip, port);
+	s.write(bw);
+    }
+
+    return total_bw / num_members;
 }
