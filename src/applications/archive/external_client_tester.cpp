0e8a333ab0eb1f422e26deeede3f933772f02cd2
Xinzhe Yang <xinzhe.y@gmail.com>
2020-01-27 01:41:01 -0500

    fixed typo

--- src/applications/archive/external_client_tester.cpp
+++ src/applications/archive/external_client_tester.cpp
@@ -22,7 +22,7 @@ int main(int argc, char** argv) {
 
     cout << "Finished constructing ExternalGroup" << endl;
 
-    std::vector<node_id_t> members = gourp.get_members();
+    std::vector<node_id_t> members = group.get_members();
     std::vector<node_id_t> shard_members = group.get_shard_members(0, 1);
     ExternalClientCaller<Foo>& foo_p2p_handle = group.get_ref<Foo>();
     {
@@ -31,7 +31,7 @@ int main(int argc, char** argv) {
     std::this_thread::sleep_for(std::chrono::seconds(3));
     {
         
-        auto result = foo_rpc_handle.p2p_send<RPC_NAME(read_state)>(0);
+        auto result = foo_p2p_handle.p2p_send<RPC_NAME(read_state)>(0);
         auto response = result.get().get(0);
         cout << "Node 0 had state = " << response << endl;
         
