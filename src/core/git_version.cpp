78c4721f7399ce39f076ceb51bb4b9937bf9796b
Edward Tremel <edward@cs.cornell.edu>
2020-06-25 17:56:23 -0400

    Updated CMake version to 2.0.0 to match tagged release version. Fixes #164

--- src/core/git_version.cpp
+++ src/core/git_version.cpp
@@ -10,11 +10,11 @@
 
 namespace derecho {
 
-const int MAJOR_VERSION = 1;
+const int MAJOR_VERSION = 2;
 const int MINOR_VERSION = 0;
 const int PATCH_VERSION = 0;
-const int COMMITS_AHEAD_OF_VERSION = 8;
-const char* VERSION_STRING = "1.0.0";
-const char* VERSION_STRING_PLUS_COMMITS = "1.0.0+8";
+const int COMMITS_AHEAD_OF_VERSION = 2;
+const char* VERSION_STRING = "2.0.0";
+const char* VERSION_STRING_PLUS_COMMITS = "2.0.0+2";
 
 }
