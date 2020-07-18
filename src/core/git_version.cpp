e290b81aa3758bfa0b4bbb04a755f45b34d8f1b1
Edward Tremel <high.order.bits@gmail.com>
2020-07-18 19:43:21 -0400

    Fixed bug in Volatile<T> constructors
    The constructors for Volatile<T> had gotten out of sync with the
    constructors for Persistent<T> that they forwarded to: The "regular" one
    neglected to forward the DeserializationManager argument, and the
    "deserialization" one used the wrong type of pointer for the log tail.

--- src/core/git_version.cpp
+++ src/core/git_version.cpp
@@ -13,8 +13,8 @@ namespace derecho {
 const int MAJOR_VERSION = 2;
 const int MINOR_VERSION = 0;
 const int PATCH_VERSION = 1;
-const int COMMITS_AHEAD_OF_VERSION = 11;
+const int COMMITS_AHEAD_OF_VERSION = 13;
 const char* VERSION_STRING = "2.0.1";
-const char* VERSION_STRING_PLUS_COMMITS = "2.0.1+11";
+const char* VERSION_STRING_PLUS_COMMITS = "2.0.1+13";
 
 }
