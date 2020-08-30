fa2dd91dde6e8cb33fc9884082f2e244b5e46822
ellerre <lorenzo.rosa.bo@gmail.com>
2020-08-30 12:37:53 -0400

    Removed null-send scheme, added missing lock and made SMC message reception as opportunistic

--- src/core/multicast_group.cpp
+++ src/core/multicast_group.cpp
@@ -663,19 +663,19 @@ void MulticastGroup::sst_receive_handler(subgroup_id_t subgroup_num, const Subgr
     locally_stable_sst_messages[subgroup_num][sequence_number] = {node_id, index, size, data};
 
     auto new_num_received = resolve_num_received(index, subgroup_settings.num_received_offset + sender_rank);
-    /* NULL Send Scheme */
-    // only if I am a sender in the subgroup and the subgroup is not in UNORDERED mode
-    if(subgroup_settings.sender_rank >= 0 && subgroup_settings.mode != Mode::UNORDERED) {
-        if(subgroup_settings.sender_rank < (int)sender_rank) {
-            while(future_message_indices[subgroup_num] <= new_num_received) {
-                get_buffer_and_send_auto_null(subgroup_num);
-            }
-        } else if(subgroup_settings.sender_rank > (int)sender_rank) {
-            while(future_message_indices[subgroup_num] < new_num_received) {
-                get_buffer_and_send_auto_null(subgroup_num);
-            }
-        }
-    }
+    // /* NULL Send Scheme */
+    // // only if I am a sender in the subgroup and the subgroup is not in UNORDERED mode
+    // if(subgroup_settings.sender_rank >= 0 && subgroup_settings.mode != Mode::UNORDERED) {
+    //     if(subgroup_settings.sender_rank < (int)sender_rank) {
+    //         while(future_message_indices[subgroup_num] <= new_num_received) {
+    //             get_buffer_and_send_auto_null(subgroup_num);
+    //         }
+    //     } else if(subgroup_settings.sender_rank > (int)sender_rank) {
+    //         while(future_message_indices[subgroup_num] < new_num_received) {
+    //             get_buffer_and_send_auto_null(subgroup_num);
+    //         }
+    //     }
+    // }
 
     if(subgroup_settings.mode == Mode::UNORDERED) {
         // issue stability upcalls for the recently sequenced messages
@@ -727,23 +727,22 @@ void MulticastGroup::receiver_function(subgroup_id_t subgroup_num, const Subgrou
     DerechoParams profile = subgroup_settings.profile;
     const uint64_t slot_width = profile.sst_max_msg_size + sizeof(uint64_t);
     std::lock_guard<std::recursive_mutex> lock(msg_state_mtx);
-    for(uint i = 0; i < batch_size; ++i) {
-        for(uint sender_count = 0; sender_count < num_shard_senders; ++sender_count) {
-            const message_id_t expected_index = sst.num_received_sst[member_index][subgroup_settings.num_received_offset + sender_count] + 1;
-            const uint32_t slot = expected_index % profile.window_size;
-            const uint32_t sender_sst_index = node_id_to_sst_index.at(
-                    subgroup_settings.members[shard_ranks_by_sender_rank.at(sender_count)]);
-            const message_id_t received_index = sst.index[sender_sst_index][subgroup_settings.index_field_index];
-            if(received_index >= expected_index) {
-                dbg_default_trace("receiver_trig calling sst_receive_handler_lambda. next_seq = {}, num_received = {}, sender rank = {}. Reading from SST row {}, slot {}",
-                                  received_index, expected_index, sender_count, sender_sst_index, subgroup_settings.slot_offset + slot_width * slot);
-                sst_receive_handler_lambda(sender_count,
-                                           &sst.slots[sender_sst_index]
-                                                     [subgroup_settings.slot_offset + slot_width * slot],
-                                           (uint64_t&)sst.slots[sender_sst_index]
-                                                               [subgroup_settings.slot_offset + slot_width * (slot + 1) - sizeof(uint64_t)]);
-                sst.num_received_sst[member_index][subgroup_settings.num_received_offset + sender_count] = expected_index;
-            }
+    for(uint sender_count = 0; sender_count < num_shard_senders; ++sender_count) {
+        const uint32_t sender_sst_index = node_id_to_sst_index.at(subgroup_settings.members[shard_ranks_by_sender_rank.at(sender_count)]);
+        uint32_t slot;
+        message_id_t old_index = sst.num_received_sst[member_index][subgroup_settings.num_received_offset + sender_count];
+        const message_id_t received_index = sst.index[sender_sst_index][subgroup_settings.index_field_index];
+        while(received_index > old_index) {
+            old_index++;
+            slot = old_index % profile.window_size;
+            dbg_default_trace("receiver_trig calling sst_receive_handler_lambda. next_seq = {}, num_received = {}, sender rank = {}. Reading from SST row {}, slot {}",
+                                received_index, old_index, sender_count, sender_sst_index, subgroup_settings.slot_offset + slot_width * slot);
+            sst_receive_handler_lambda(sender_count,
+                                        &sst.slots[sender_sst_index]
+                                                  [subgroup_settings.slot_offset + slot_width * slot],
+                                        (uint64_t&)sst.slots[sender_sst_index]
+                                                            [subgroup_settings.slot_offset + slot_width * (slot + 1) - sizeof(uint64_t)]);
+            sst.num_received_sst[member_index][subgroup_settings.num_received_offset + sender_count] = old_index;
         }
     }
     sst.put((char*)std::addressof(sst.num_received_sst[0][subgroup_settings.num_received_offset]) - sst.getBaseAddress(),
@@ -831,6 +830,7 @@ void MulticastGroup::delivery_trigger(subgroup_id_t subgroup_num, const Subgroup
 }
 void MulticastGroup::sst_send_trigger(subgroup_id_t subgroup_num, const SubgroupSettings& subgroup_settings,
                                       const uint32_t num_shard_members, DerechoSST& sst) {
+    std::lock_guard<std::recursive_mutex> lock(msg_state_mtx);
     // to avoid a race condition, do not read the same counter twice
     int32_t current_committed_sst_index = committed_sst_index[subgroup_num];
     // sst.index[member_index][subgroup_settings.index_field_index] contains the old counter value
