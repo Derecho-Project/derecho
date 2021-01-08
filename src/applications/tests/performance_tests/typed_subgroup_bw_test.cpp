27800b3c5211ea68e3b8658bda49534e34f4478f
ZhengPeterWang <zw494@cornell.edu>
2021-01-08 09:42:31 -0500

    modified parts of the typed test, see if this version works

--- src/applications/tests/performance_tests/typed_subgroup_bw_test.cpp
+++ src/applications/tests/performance_tests/typed_subgroup_bw_test.cpp
@@ -2,8 +2,8 @@
 #include <map>
 #include <memory>
 #include <string>
-#include <time.h>
 #include <vector>
+#include <chrono>
 
 #include "aggregate_bandwidth.cpp"
 #include "aggregate_bandwidth.hpp"
@@ -18,7 +18,7 @@
 
 using std::endl;
 using test::Bytes;
-
+using namespace std::chrono;
 /**
  * RPC Object with a single function that accepts a string
  */
@@ -107,11 +107,14 @@ int main(int argc, char* argv[]) {
             total_num_messages = count;
             break;
     }
-    
+   
+    steady_clock::time_point begin_time, send_complete_time;
+
     // variable 'done' tracks the end of the test
     volatile bool done = false;
     // callback into the application code at each message delivery
     auto stability_callback = [&done,
+                               &send_complete_time,
                                total_num_messages,
                                num_delivered = 0u](uint32_t subgroup,
                                                    uint32_t sender_id,
@@ -122,7 +125,8 @@ int main(int argc, char* argv[]) {
         ++num_delivered;
         // Check for completion
         if(num_delivered == total_num_messages) {
-            done = true;
+            send_complete_time = std::chrono::steady_clock::now();
+	    done = true;
         }
     };
 
@@ -132,63 +136,35 @@ int main(int argc, char* argv[]) {
         pthread_setname_np(pthread_self(), DEFAULT_PROC_NAME);
     }
 
-    derecho::SubgroupInfo subgroup_info{[num_nodes](
-                                                const std::vector<std::type_index>& subgroup_type_order,
-                                                const std::unique_ptr<derecho::View>& prev_view, derecho::View& curr_view) {
-        if(curr_view.num_members < num_nodes) {
-            std::cout << "not enough members yet:" << curr_view.num_members << " < " << num_nodes << std::endl;
-            throw derecho::subgroup_provisioning_exception();
-        }
-        derecho::subgroup_shard_layout_t subgroup_layout(1);
-
-        std::vector<uint32_t> members(num_nodes);
-        for(int i = 0; i < num_nodes; i++) {
-            members[i] = i;
-        }
-
-        subgroup_layout[0].emplace_back(curr_view.make_subview(members));
-        curr_view.next_unassigned_rank = std::max(curr_view.next_unassigned_rank, num_nodes);
-        derecho::subgroup_allocation_map_t subgroup_allocation;
-        subgroup_allocation.emplace(std::type_index(typeid(TestObject)), std::move(subgroup_layout));
-        return subgroup_allocation;
-    }};
+    derecho::SubgroupInfo subgroup_info(PartialSendersAllocator(num_nodes,senders_mode)); 
 
     auto ba_factory = [](persistent::PersistentRegistry*, derecho::subgroup_id_t) { return std::make_unique<TestObject>(); };
 
     derecho::Group<TestObject> group(derecho::CallbackSet{stability_callback}, subgroup_info, {}, std::vector<derecho::view_upcall_t>{}, ba_factory);
     std::cout << "Finished constructing/joining Group" << std::endl;
 
-    derecho::Replicated<TestObject>& handle = group.get_subgroup<TestObject>();
     //std::string str_1k(max_msg_size, 'x');
     char* bbuf = (char*)malloc(max_msg_size);
     bzero(bbuf, max_msg_size);
     Bytes bytes(bbuf, max_msg_size);
 
-    struct timespec t1, t2;
-    clock_gettime(CLOCK_REALTIME, &t1);
-
-    // this function sends all the messages
-    auto send_all = [&]() {
-        for(uint i = 0; i < count; i++) {
-        //handle.ordered_send<RPC_NAME(fun)>(str_1k);
-        handle.ordered_send<RPC_NAME(bytes_fun)>(bytes);
-    }
-    };
-
     int node_rank = group.get_my_rank();
 
-    if(senders_mode == PartialSendMode::ALL_SENDERS) {
-        send_all();
-    } else if(senders_mode == PartialSendMode::HALF_SENDERS) {
-        if(node_rank > (num_nodes - 1) / 2) {
-            send_all();
-        }
-    } else {
-        if(node_rank == num_nodes - 1) {
-            send_all();
-        }
+    bool is_sending = true;
+    if((senders_mode == PartialSendMode::HALF_SENDERS) && (node_rank <= (num_nodes - 1) / 2)) {
+        is_sending = false;
+    }
+    if((senders_mode == PartialSendMode::ONE_SENDER) && (node_rank != num_nodes - 1)) {
+        is_sending = false;
     }
 
+    begin_time = std::chrono::steady_clock::now();
+    if (is_sending){
+        derecho::Replicated<TestObject>& handle = group.get_subgroup<TestObject>();
+        for (uint i = 0; i < count; i++){
+            handle.ordered_send<RPC_NAME(bytes_fun)>(bytes);
+        }
+    }
     /*
     if(node_rank == 0) {
         derecho::rpc::QueryResults<bool> results = handle.ordered_send<RPC_NAME(finishing_call)>(0);
@@ -202,22 +178,14 @@ int main(int argc, char* argv[]) {
     while(!done){
     }
 
-    clock_gettime(CLOCK_REALTIME, &t2);
     free(bbuf);
 
-    double thp_gbps;
-    int64_t nsec = ((int64_t)t2.tv_sec - t1.tv_sec) * 1000000000 + t2.tv_nsec - t1.tv_nsec;
+    int64_t nsec = duration_cast<nanoseconds>(send_complete_time - begin_time).count(); 
 
-    if(senders_mode == PartialSendMode::ALL_SENDERS) {
-        thp_gbps = ((double)count * max_msg_size * num_nodes) / nsec;
-    } else if(senders_mode == PartialSendMode::HALF_SENDERS) {
-        thp_gbps = ((double)count * max_msg_size * num_nodes/2) / nsec;
-    } else {
-        thp_gbps = ((double)count * max_msg_size) / nsec;
-    }
+    double thp_gbps = (static_cast<double>(total_num_messages) * max_msg_size) / nsec;
+    double thp_ops = (static_cast<double>(total_num_messages) * 1000000000) / nsec;
 
-    double msec = (double)nsec / 1000000;
-    double thp_ops = ((double)count * 1000000000) / nsec;
+    double msec = static_cast<double>(nsec) / 1000000;
     std::cout << "timespan:" << msec << " millisecond." << std::endl;
     std::cout << "throughput:" << thp_gbps << "GB/s." << std::endl;
     std::cout << "throughput:" << thp_ops << "ops." << std::endl;
