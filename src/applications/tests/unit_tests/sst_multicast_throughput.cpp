4f0d700ec351baf79c137579915f2a970cf4dcd7
Edward Tremel <high.order.bits@gmail.com>
2021-01-19 13:36:38 -0500

    Minor readability improvement
    Converted the condition-with-side-effects in the SST Multicast test's
    main loop to a more standard while loop, and added a few more comments.

--- src/applications/tests/unit_tests/sst_multicast_throughput.cpp
+++ src/applications/tests/unit_tests/sst_multicast_throughput.cpp
@@ -70,6 +70,7 @@ int main(int argc, char* argv[]) {
         members.push_back(p.first);
     }
 
+    // compute row_offset, based on the user's selected number of senders
     uint32_t num_senders = num_nodes, row_offset = 0;
     if(num_senders_selector == 0) {
     } else if(num_senders_selector == 1) {
@@ -158,6 +159,7 @@ int main(int argc, char* argv[]) {
     // insert the predicate later
 
     // Configure the senders based on the user's input
+    // In half-senders and one-sender mode, the lower-ranked nodes are set to non-sending
     std::vector<uint32_t> indices(num_nodes);
     std::iota(indices.begin(), indices.end(), 0);
     std::vector<int> is_sender(num_nodes, 1);
@@ -180,7 +182,6 @@ int main(int argc, char* argv[]) {
     // now insert the predicate
     sst->predicates.insert(receiver_pred, receiver_trig,
                            sst::PredicateType::RECURRENT);
-    // uint count = 0;
     struct timespec start_time, end_time;
     // start timer
     clock_gettime(CLOCK_REALTIME, &start_time);
@@ -188,9 +189,9 @@ int main(int argc, char* argv[]) {
        || (node_rank > (num_nodes - 1) / 2 && num_senders_selector == 1)) {
         for(uint i = 0; i < num_messages; ++i) {
             // Get the send buffer
-            volatile char* buf;
-            while((buf = group.get_buffer(max_msg_size)) == NULL) {
-                // ++count;
+            volatile char* buf = NULL;
+            while(buf == NULL) {
+                buf = group.get_buffer(max_msg_size);
             }
             // Send a nonsense message
             // for(uint i = 0; i < size; ++i) {
@@ -204,9 +205,10 @@ int main(int argc, char* argv[]) {
     }
     // end timer
     clock_gettime(CLOCK_REALTIME, &end_time);
+    // compute statistics
     double my_time = ((end_time.tv_sec * 1e9 + end_time.tv_nsec) - (start_time.tv_sec * 1e9 + start_time.tv_nsec));
     double message_rate = (num_messages * 1e9) / my_time;
-    ;
+
     if(num_senders_selector == 0) {
         message_rate *= num_nodes;
     } else if(num_senders_selector == 1) {
