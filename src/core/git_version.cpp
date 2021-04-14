d6e1b19c4b997e71e81db1926ed82d3b0a70c102
Edward Tremel <high.order.bits@gmail.com>
2021-04-14 14:18:10 -0400

    Fixed a bug in RPCManager's handling of P2P requests
    This is a re-application of commit
    669e03ddea24951cdeb50e213543722987c21d94 into master to import this
    bugfix without merging.
    
    Since p2p_request_worker runs in a while(!thread_shutdown) loop, but the
    local variable reply_size is declared outside the loop, reply_size is
    only initialized once and then gets reused on every iteration of the
    loop. This means if(reply_size > 0) will not correctly detect whether
    receive_message populated the reply buffer - if no reply was generated,
    reply_size will be left at whatever value it had during the last loop
    iteration. This means the "hack" in the else clause does not necessarily
    get to run if the function did not generate a reply.

--- src/core/git_version.cpp
+++ src/core/git_version.cpp
@@ -13,8 +13,8 @@ namespace derecho {
 const int MAJOR_VERSION = 2;
 const int MINOR_VERSION = 1;
 const int PATCH_VERSION = 0;
-const int COMMITS_AHEAD_OF_VERSION = 29;
+const int COMMITS_AHEAD_OF_VERSION = 91;
 const char* VERSION_STRING = "2.1.0";
-const char* VERSION_STRING_PLUS_COMMITS = "2.1.0+29";
+const char* VERSION_STRING_PLUS_COMMITS = "2.1.0+91";
 
 }
