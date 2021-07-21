Derecho-Project/derecho

commit 163750226994a084b52060ebe5f9393d4e6fc63c

Added a std::future for the signature verification event
Using the same framework as the futures for local and global
persistence, I added a future to PendingResults/QueryResults
representing the "global verification" event (all replicas have valid
signatures) that is triggered by an internal callback to RPCManager.
This allows the signed_store_test and signed_store_mockup programs to
eliminate the "CompletionTracker" crutch entirely, and probably finishes
fixing issue #167 (it should still be tested, though).

src/core/rpc_manager.cpp
@@ -67,10 +67,10 @@ void RPCManager::destroy_remote_invocable_class(uint32_t instance_id) {
         pending_results_to_fulfill[instance_id].front().get().set_exception_for_caller_removed();
         pending_results_to_fulfill[instance_id].pop();
     }
-    for(auto& pending_results_pair : results_pending_local_persistence[instance_id]) {
+    for(auto& pending_results_pair : results_awaiting_local_persistence[instance_id]) {
         pending_results_pair.second.get().set_exception_for_caller_removed();
     }
-    results_pending_local_persistence[instance_id].clear();
+    results_awaiting_local_persistence[instance_id].clear();
 }
 
 void RPCManager::start_listening() {
@@ -158,9 +158,13 @@ void RPCManager::rpc_message_handler(subgroup_id_t subgroup_id, node_id_t sender
             pending_results_to_fulfill[subgroup_id].front().get().fulfill_map(
                     view_manager.unsafe_get_current_view().subgroup_shard_views.at(subgroup_id).at(my_shard).members);
             pending_results_to_fulfill[subgroup_id].front().get().set_persistent_version(version, timestamp);
-            dbg_default_debug("RPCManager: Moving PendingResults for version {} to pending_local_persistence", version);
-            results_pending_local_persistence[subgroup_id].emplace(version,
-                                                                   std::move(pending_results_to_fulfill[subgroup_id].front()));
+            //Move the fulfilled PendingResults to either the "completed" list or the "awaiting persistence" list
+            if(view_manager.subgroup_is_persistent(subgroup_id)) {
+                results_awaiting_local_persistence[subgroup_id].emplace(version,
+                                                                       std::move(pending_results_to_fulfill[subgroup_id].front()));
+            } else {
+                completed_pending_results[subgroup_id].emplace_back(std::move(pending_results_to_fulfill[subgroup_id].front()));
+            }
             pending_results_to_fulfill[subgroup_id].pop();
         }  //release pending_results_mutex
         if(reply_size > 0) {
@@ -223,17 +227,13 @@ void RPCManager::new_view_callback(const View& new_view) {
     }
     dbg_default_debug("Created new connections among the new view members");
     std::lock_guard<std::mutex> lock(pending_results_mutex);
-    for(auto& fulfilled_pending_results_pair : results_pending_local_persistence) {
+    for(auto& fulfilled_pending_results_pair : results_awaiting_local_persistence) {
         const subgroup_id_t subgroup_id = fulfilled_pending_results_pair.first;
         //For each PendingResults in this subgroup, check the departed list of each shard in
         //the subgroup, and call set_exception_for_removed_node for the departed nodes
         for(auto pending_results_iter = fulfilled_pending_results_pair.second.begin();
             pending_results_iter != fulfilled_pending_results_pair.second.end();) {
-            //Garbage-collect PendingResults references that are obsolete
-            if(pending_results_iter->second.get().all_responded()) {
-                // Only remove them if they are RPCs for non-persistent subgroups...how to determine that?
-                // pending_results_iter = fulfilled_pending_results_pair.second.erase(pending_results_iter);
-            } else {
+            if(!pending_results_iter->second.get().all_responded()) {
                 for(uint32_t shard_num = 0;
                     shard_num < new_view.subgroup_shard_views[subgroup_id].size();
                     ++shard_num) {
@@ -248,8 +248,8 @@ void RPCManager::new_view_callback(const View& new_view) {
             }
         }
     }
-    //Do the same departed-node check on PendingResults in the pending_global_persistence map
-    for(auto& fulfilled_pending_results_pair : results_pending_global_persistence) {
+    //Do the same departed-node check on PendingResults in the awaiting_global_persistence map
+    for(auto& fulfilled_pending_results_pair : results_awaiting_global_persistence) {
         const subgroup_id_t subgroup_id = fulfilled_pending_results_pair.first;
         for(auto pending_results_iter = fulfilled_pending_results_pair.second.begin();
             pending_results_iter != fulfilled_pending_results_pair.second.end();) {
@@ -266,6 +266,10 @@ void RPCManager::new_view_callback(const View& new_view) {
             }
         }
     }
+
+    //No need to check any entries in the awaiting_signature map - if an update has reached global
+    //persistence, then all of the replicas must have responded to the RPC message
+
     //Do the same check on completed_pending_results, but remove them if they are now finished
     for(auto& fulfilled_pending_results_pair : completed_pending_results) {
         const subgroup_id_t subgroup_id = fulfilled_pending_results_pair.first;
@@ -295,13 +299,13 @@ void RPCManager::notify_persistence_finished(subgroup_id_t subgroup_id, persiste
     std::lock_guard<std::mutex> lock(pending_results_mutex);
     //PendingResults in each per-subgroup map are ordered by version number, so all entries before
     //the argument version number have been persisted and need to be notified
-    for(auto pending_results_iter = results_pending_local_persistence[subgroup_id].begin();
-        pending_results_iter != results_pending_local_persistence[subgroup_id].upper_bound(version);) {
+    for(auto pending_results_iter = results_awaiting_local_persistence[subgroup_id].begin();
+        pending_results_iter != results_awaiting_local_persistence[subgroup_id].upper_bound(version);) {
         dbg_default_debug("RPCManager: Setting local persistence on version {}", pending_results_iter->first);
         pending_results_iter->second.get().set_local_persistence();
-        //Move the PendingResults reference to results_pending_global_persistence, with the same key
-        results_pending_global_persistence[subgroup_id].emplace(std::move(*pending_results_iter));
-        pending_results_iter = results_pending_local_persistence[subgroup_id].erase(pending_results_iter);
+        //Move the PendingResults reference to results_awaiting_global_persistence, with the same key
+        results_awaiting_global_persistence[subgroup_id].emplace(std::move(*pending_results_iter));
+        pending_results_iter = results_awaiting_local_persistence[subgroup_id].erase(pending_results_iter);
     }
 }
 
@@ -310,13 +314,31 @@ void RPCManager::notify_global_persistence_finished(subgroup_id_t subgroup_id, p
     std::lock_guard<std::mutex> lock(pending_results_mutex);
     //PendingResults in each per-subgroup map are ordered by version number, so all entries before
     //the argument version number have been persisted and need to be notified
-    for(auto pending_results_iter = results_pending_global_persistence[subgroup_id].begin();
-        pending_results_iter != results_pending_global_persistence[subgroup_id].upper_bound(version);) {
+    for(auto pending_results_iter = results_awaiting_global_persistence[subgroup_id].begin();
+        pending_results_iter != results_awaiting_global_persistence[subgroup_id].upper_bound(version);) {
         dbg_default_debug("RPCManager: Setting global persistence on version {}", pending_results_iter->first);
         pending_results_iter->second.get().set_global_persistence();
+        //Move the PendingResults reference to results_awaiting_signature if the subgroup needs signatures,
+        //or completed_pending_results if it does not
+        if(view_manager.subgroup_is_signed(subgroup_id)) {
+            results_awaiting_signature[subgroup_id].emplace(std::move(*pending_results_iter));
+        } else {
+            completed_pending_results[subgroup_id].emplace_back(std::move(pending_results_iter->second));
+        }
+        pending_results_iter = results_awaiting_global_persistence[subgroup_id].erase(pending_results_iter);
+    }
+}
+
+void RPCManager::notify_verification_finished(subgroup_id_t subgroup_id, persistent::version_t version) {
+    dbg_default_debug("RPCManager: Got a global verification callback for version {}", version);
+    std::lock_guard<std::mutex> lock(pending_results_mutex);
+    for(auto pending_results_iter = results_awaiting_signature[subgroup_id].begin();
+        pending_results_iter != results_awaiting_signature[subgroup_id].upper_bound(version);) {
+        dbg_default_debug("RPCManager: Setting signature verification on version {}", pending_results_iter->first);
+        pending_results_iter->second.get().set_signature_verified();
         //Move the PendingResults reference to completed_pending_results
         completed_pending_results[subgroup_id].emplace_back(std::move(pending_results_iter->second));
-        pending_results_iter = results_pending_global_persistence[subgroup_id].erase(pending_results_iter);
+        pending_results_iter = results_awaiting_signature[subgroup_id].erase(pending_results_iter);
     }
 }
 

include/derecho/core/detail/group_impl.hpp
@@ -195,6 +195,10 @@ Group<ReplicatedTypes...>::Group(const UserMessageCallbacks& callbacks,
             //Global persistence callback
             [this](subgroup_id_t subgroup, persistent::version_t version) {
                 rpc_manager.notify_global_persistence_finished(subgroup, version);
+            },
+            //Verification callback
+            [this](subgroup_id_t subgroup, persistent::version_t version) {
+                rpc_manager.notify_verification_finished(subgroup, version);
             }};
     view_manager.initialize_multicast_groups(callbacks, internal_callbacks);
     rpc_manager.create_connections();

include/derecho/core/detail/multicast_group.hpp
@@ -265,6 +265,12 @@ struct MulticastGroupCallbacks {
      * callback in UserMessageCallbacks).
      */
     persistence_callback_t global_persistence_callback;
+    /**
+     * A callback to notify internal components that a new version has been
+     * signed and verified on all replicas (separate from the user-defined
+     * verification callback in UserMessageCallbacks).
+     */
+    verified_callback_t global_verified_callback;
 };
 
 /** Implements the low-level mechanics of tracking multicasts in a Derecho group,

include/derecho/core/detail/rpc_manager.hpp
@@ -126,14 +126,23 @@ class RPCManager {
      * version number). These RPC messages have been delivered locally but RPCManager
      * still needs to use the PendingResults to report that persistence has finished.
      */
-    std::map<subgroup_id_t, std::map<persistent::version_t, PendingBase_ref>> results_pending_local_persistence;
+    std::map<subgroup_id_t, std::map<persistent::version_t, PendingBase_ref>> results_awaiting_local_persistence;
     /**
      * For each subgroup, contains a map from version number to the PendingResults
      * for that version's RPC call (i.e., a set of PendingResults indexed by
      * version number). These RPC messages have been persisted locally but RPCManager
      * still needs to use the PendingResults to report that global persistence has finished.
      */
-    std::map<subgroup_id_t, std::map<persistent::version_t, PendingBase_ref>> results_pending_global_persistence;
+    std::map<subgroup_id_t, std::map<persistent::version_t, PendingBase_ref>> results_awaiting_global_persistence;
+    /**
+     * For each subgroup, contains a map from version number to the PendingResults
+     * for that version's RPC call (i.e., a set of PendingResults indexed by
+     * version number). These RPC messages have finished global persistence but
+     * were sent to subgroups with signatures enabled, so RPCManager still
+     * needs to use the PendingResults to report that the signature
+     * verification has finished.
+     */
+    std::map<subgroup_id_t, std::map<persistent::version_t, PendingBase_ref>> results_awaiting_signature;
     /**
      * For each subgroup, contains a list of PendingResults references for RPC
      * messages that have completed all of their promise events (fulfilling
@@ -334,6 +343,8 @@ public:
      * finished persisting in that subgroup
      */
     void notify_global_persistence_finished(subgroup_id_t subgroup_id, persistent::version_t version);
+
+    void notify_verification_finished(subgroup_id_t subgroup_id, persistent::version_t version);
 
     /**
      * Sends the next message in the MulticastGroup's send buffer (which is

include/derecho/core/detail/rpc_utils.hpp
@@ -272,8 +272,10 @@ private:
     std::future<std::pair<persistent::version_t, uint64_t>> persistent_version;
     /** This signals that local persistence has completed for the version assigned to this RPC function call */
     std::future<void> local_persistence_done;
-    /** This signals that local persistence has completed for the version assigned to this RPC function call */
+    /** This signals that global persistence has completed for the version assigned to this RPC function call */
     std::future<void> global_persistence_done;
+    /** This signals that the signature has been verified at all replicas on the version assigned to this RPC function call */
+    std::future<void> signature_done;
 
 public:
     /**
@@ -283,18 +285,21 @@ public:
      * PendingResults that constructed this QueryResults.
      */
     QueryResults(map_fut reply_map_future, std::future<std::pair<persistent::version_t, uint64_t>> persistent_version,
-                 std::future<void> local_persistence_done, std::future<void> global_persistence_done)
+                 std::future<void> local_persistence_done, std::future<void> global_persistence_done,
+                 std::future<void> signature_done)
             : pending_rmap(std::move(reply_map_future)),
               persistent_version(std::move(persistent_version)),
               local_persistence_done(std::move(local_persistence_done)),
-              global_persistence_done(std::move(global_persistence_done)) {}
+              global_persistence_done(std::move(global_persistence_done)),
+              signature_done(std::move(signature_done)) {}
     /** Move constructor for QueryResults. */
     QueryResults(QueryResults&& o)
             : pending_rmap{std::move(o.pending_rmap)},
               replies{std::move(o.replies)},
               persistent_version{std::move(o.persistent_version)},
               local_persistence_done{std::move(o.local_persistence_done)},
-              global_persistence_done{std::move(o.global_persistence_done)} {}
+              global_persistence_done{std::move(o.global_persistence_done)},
+              signature_done{std::move(o.signature_done)} {}
     /** QueryResults, like std::future, is not copyable. */
     QueryResults(const QueryResults&) = delete;
 
@@ -362,6 +367,16 @@ public:
     void await_global_persistence() {
         global_persistence_done.get();
     }
+
+    /**
+     * Blocks until the update caused by this RPC function call has been signed
+     * on all replicas and the signatures have been verified. Note that this is
+     * meaningless if signed updates are not enabled, and it will only work on
+     * QueryResults that are generated by ordered_send calls.
+     */
+    void await_signature_verification() {
+        signature_done.get();
+    }
 };
 
 /**
@@ -412,22 +427,27 @@ private:
     std::future<std::pair<persistent::version_t, uint64_t>> persistent_version;
     /** This signals that local persistence has completed for the version assigned to this RPC function call */
     std::future<void> local_persistence_done;
-    /** This signals that local persistence has completed for the version assigned to this RPC function call */
+    /** This signals that global persistence has completed for the version assigned to this RPC function call */
     std::future<void> global_persistence_done;
+    /** This signals that the signature has been verified at all replicas on the version assigned to this RPC function call */
+    std::future<void> signature_done;
 
 public:
     QueryResults(map_fut reply_map_future, std::future<std::pair<persistent::version_t, uint64_t>> persistent_version,
-                 std::future<void> local_persistence_done, std::future<void> global_persistence_done)
+                 std::future<void> local_persistence_done, std::future<void> global_persistence_done,
+                 std::future<void> signature_done)
             : pending_rmap(std::move(reply_map_future)),
               persistent_version(std::move(persistent_version)),
               local_persistence_done(std::move(local_persistence_done)),
-              global_persistence_done(std::move(global_persistence_done)) {}
+              global_persistence_done(std::move(global_persistence_done)),
+              signature_done(std::move(signature_done)) {}
     QueryResults(QueryResults&& o)
             : pending_rmap{std::move(o.pending_rmap)},
               replies{std::move(o.replies)},
               persistent_version{std::move(o.persistent_version)},
               local_persistence_done{std::move(o.local_persistence_done)},
-              global_persistence_done{std::move(o.global_persistence_done)} {}
+              global_persistence_done{std::move(o.global_persistence_done)},
+              signature_done{std::move(o.signature_done)} {}
     QueryResults(const QueryResults&) = delete;
 
     /**
@@ -487,6 +507,14 @@ public:
     void await_global_persistence() {
         global_persistence_done.get();
     }
+
+    /**
+     * Blocks until the update caused by this RPC function call has been signed
+     * on all replicas and the signatures have been verified.
+     */
+    void await_signature_verification() {
+        signature_done.get();
+    }
 };
 
 /**
@@ -500,6 +528,7 @@ public:
     virtual void set_persistent_version(persistent::version_t, uint64_t) = 0;
     virtual void set_local_persistence() = 0;
     virtual void set_global_persistence() = 0;
+    virtual void set_signature_verified() = 0;
     virtual void set_exception_for_removed_node(const node_id_t&) = 0;
     virtual void set_exception_for_caller_removed() = 0;
     virtual bool all_responded() const = 0;
@@ -566,6 +595,11 @@ private:
      * std::promise if set_value() has been called on it.
      */
     bool global_persistence_finished = false;
+    /**
+     * A promise representing the "signature verified" event for the update
+     * caused by this RPC call; the future end lives in QueryResults.
+     */
+    std::promise<void> signature_verified_promise;
 
 public:
     PendingResults()
@@ -581,7 +615,8 @@ public:
         return QueryResults<Ret>{promise_for_pending_map.get_future(),
                                  version_promise.get_future(),
                                  local_persistence_promise.get_future(),
-                                 global_persistence_promise.get_future()};
+                                 global_persistence_promise.get_future(),
+                                 signature_verified_promise.get_future()};
     }
 
     /**
@@ -626,10 +661,18 @@ public:
         }
     }
 
+    /**
+     * Fulfills a promise for a single node's reply to indicate that the node
+     * will never reply, by putting a node_removed_from_group_exception in the
+     * promise. This happens if the node is removed in a View change while the
+     * RPC is still awaiting its reply.
+     */
     void set_exception_for_removed_node(const node_id_t& removed_nid) {
         assert(map_fulfilled);
         if(dest_nodes.find(removed_nid) != dest_nodes.end()
            && responded_nodes.find(removed_nid) == responded_nodes.end()) {
+            //Mark the node as "responded" for the purposes of the other methods
+            responded_nodes.insert(removed_nid);
             set_exception(removed_nid,
                           std::make_exception_ptr(
                                   node_removed_from_group_exception{removed_nid}));
@@ -712,6 +755,15 @@ public:
     }
 
     /**
+     * Fulfills the signature verification promise, unblocking the future end.
+     * This should be called to signal client code that the update has been
+     * correctly signed on all replicas.
+     */
+    void set_signature_verified() {
+        signature_verified_promise.set_value();
+    }
+
+    /**
      * reset this object.
      */
     void reset() {
@@ -726,6 +778,7 @@ public:
         version_promise = std::promise<std::pair<persistent::version_t, uint64_t>>();
         local_persistence_promise = std::promise<void>();
         global_persistence_promise = std::promise<void>();
+        signature_verified_promise = std::promise<void>();
     }
 };
 
@@ -744,13 +797,15 @@ private:
     std::promise<std::pair<persistent::version_t, uint64_t>> version_promise;
     std::promise<void> local_persistence_promise;
     std::promise<void> global_persistence_promise;
+    std::promise<void> signature_verified_promise;
 
 public:
     QueryResults<void> get_future() {
         return QueryResults<void>(promise_for_pending_map.get_future(),
                                   version_promise.get_future(),
                                   local_persistence_promise.get_future(),
-                                  global_persistence_promise.get_future());
+                                  global_persistence_promise.get_future(),
+                                  signature_verified_promise.get_future());
     }
 
     void fulfill_map(const node_list_t& sent_nodes) {
@@ -802,12 +857,22 @@ public:
         global_persistence_promise.set_value();
     }
 
+    /**
+     * Fulfills the signature verification promise, unblocking the future end.
+     * This should be called to signal client code that the update has been
+     * correctly signed on all replicas.
+     */
+    void set_signature_verified() {
+        signature_verified_promise.set_value();
+    }
+
     void reset() {
         promise_for_pending_map = std::promise<std::unique_ptr<std::set<node_id_t>>>();
         map_fulfilled = false;
         version_promise = std::promise<std::pair<persistent::version_t, uint64_t>>();
         local_persistence_promise = std::promise<void>();
         global_persistence_promise = std::promise<void>();
+        signature_verified_promise = std::promise<void>();
     }
 };
 

include/derecho/core/detail/view_manager.hpp
@@ -802,6 +802,7 @@ public:
     /** Returns a vector of vectors listing the members of a single subgroup
      * (identified by type and index), organized by shard number. */
     std::vector<std::vector<node_id_t>> get_subgroup_members(subgroup_type_id_t subgroup_type, uint32_t subgroup_index);
+    /** Returns the number of shards in a subgroup, identified by its type and index. */
     std::size_t get_number_of_shards_in_subgroup(subgroup_type_id_t subgroup_type, uint32_t subgroup_index);
     /**
      * If this node is a member of the given subgroup (identified by its type
@@ -809,9 +810,31 @@ public:
      * Otherwise, returns -1.
      */
     int32_t get_my_shard(subgroup_type_id_t subgroup_type, uint32_t subgroup_index);
-    /** Instructs the managed DerechoGroup's to send the next message. This
-     * returns immediately in sending through RDMC; the send is scheduled to happen some time in the future.
-     * if sending through SST, the RDMA write is issued in this call*/
+
+    /**
+     * Determines whether a subgroup (identified by its ID) uses persistence.
+     * Used by RPCManager, which doesn't have direct access to the Replicated
+     * Objects represented by subgroups.
+     * @return true if the subgroup represents a Replicated Object with
+     * Persistent<T> fields, false otherwise
+     */
+    bool subgroup_is_persistent(subgroup_id_t subgroup_num) const;
+
+    /**
+     * Determines whether a subgroup (identified by its ID) has signatures
+     * enabled. Used by RPCManager, which doesn't have direct access to the
+     * Replicated Objects.
+     * @return true if the subgroup represents a Replicated Object with a
+     * signatures enabled, false otherwise
+     */
+    bool subgroup_is_signed(subgroup_id_t subgroup_num) const;
+
+    /**
+     * Instructs the managed MulticastGroup to send a message. This returns
+     * immediately if sending through RDMC; the send is scheduled to happen
+     * some time in the future. If sending through SST, the RDMA write is
+     * issued in this call.
+     */
     void send(subgroup_id_t subgroup_num, long long unsigned int payload_size,
               const std::function<void(char* buf)>& msg_generator, bool cooked_send = false);
 

commit 25df1d228b684138c021ae1c5900b0f82397ad11

Fixed a bug in RPCManager introduced by 163750226994a084b52060ebe5f9393d4e6fc63c
I can't believe I didn't notice this earlier, but the iterator increment
in new_view_callback needs to be outside the if statement now that there
is no conditional branch that could erase from the map (instead of
iterating). This would cause an infinite loop if these maps were ever
non-empty during a view change.

src/core/rpc_manager.cpp
@@ -243,8 +243,8 @@ void RPCManager::new_view_callback(const View& new_view) {
                         pending_results_iter->second.get().set_exception_for_removed_node(removed_id);
                     }
                 }
-                pending_results_iter++;
             }
+            pending_results_iter++;
         }
     }
     //Do the same departed-node check on PendingResults in the awaiting_global_persistence map
@@ -261,8 +261,8 @@ void RPCManager::new_view_callback(const View& new_view) {
                         pending_results_iter->second.get().set_exception_for_removed_node(removed_id);
                     }
                 }
-                pending_results_iter++;
             }
+            pending_results_iter++;
         }
     }
 