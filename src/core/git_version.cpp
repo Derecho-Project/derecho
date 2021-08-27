d3aef870d92d461a4599a296c679543e94a45c1a
Edward Tremel <high.order.bits@gmail.com>
2021-08-27 13:04:04 -0400

    Updated CMakeLists version to 2.2.1
    This will allow me to create a tag for release 2.2.1.

--- src/core/git_version.cpp
+++ src/core/git_version.cpp
@@ -12,9 +12,9 @@ namespace derecho {
 
 const int MAJOR_VERSION = 2;
 const int MINOR_VERSION = 2;
-const int PATCH_VERSION = 0;
-const int COMMITS_AHEAD_OF_VERSION = 23;
-const char* VERSION_STRING = "2.2.0";
-const char* VERSION_STRING_PLUS_COMMITS = "2.2.0+23";
+const int PATCH_VERSION = 1;
+const int COMMITS_AHEAD_OF_VERSION = 0;
+const char* VERSION_STRING = "2.2.1";
+const char* VERSION_STRING_PLUS_COMMITS = "2.2.1+0";
 
 }
