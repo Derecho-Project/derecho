7b790269940d61706a249c65ac442d4e672e036f
Weijia <songweijia@gmail.com>
2021-09-17 21:39:30 -0400

    +GroupProjection::get_my_id()

--- include/derecho/core/group.hpp
+++ include/derecho/core/group.hpp
@@ -93,6 +93,7 @@ protected:
     virtual void set_replicated_pointer(std::type_index, uint32_t, void**) = 0;
     virtual void set_external_caller_pointer(std::type_index, uint32_t, void**) = 0;
     virtual ViewManager& get_view_manager() = 0;
+    virtual node_id_t get_my_id() = 0;
 
 public:
     Replicated<ReplicatedType>& get_subgroup(uint32_t subgroup_num = 0);
