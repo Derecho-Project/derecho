66da783f1b09c12328ed7fafa40ccd86835b8fae
Wiijia Song <songweijia@gmail.com>
2022-03-07 14:12:12 -0500

    +comments

--- include/derecho/core/register_rpc_functions.hpp
+++ include/derecho/core/register_rpc_functions.hpp
@@ -10,22 +10,6 @@
 #define applyordered_(x) make_ordered_tagger_expr(x),
 #define applyordered(...) EVAL(MAP(applyordered_, __VA_ARGS__))
 
-/**
- * This macro automatically generates a register_functions() method for a Derecho
- * Replicated Object. Example usage for a class Thing with methods foo() and bar():
- *
- * REGISTER_RPC_FUNCTIONS(Thing, ORDERED_TARGETS(foo), P2P_TARGETS(bar))
- *
- * The macro invocation belongs in your class definition, in the "public:" section.
- *
- * @param name The name of the class that is being declared as a Replicated Object
- * @param arg1 Either an invocation of the ORDERED_TARGETS macro containing the
- * names of each class method that should be callable by an ordered send, or an
- * invocation of the P2P_TARGETS macro containing the names of each class method
- * that should be callable by a P2P send.
- * @param arg2 Either ORDERED_TARGETS or P2P_TARGETS, just like arg1
- *
-*/
 #define REGISTER_RPC_FUNCTIONS_NARGS_(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,_21,_22,_23,_24,_25,_26,_27,_28,_29,_30,_31,_32,_33,_34,_35,_36,_37,_38,_39,_40,_41,_42,_43,_44,_45,_46,_47,_48,_49,_50,_51,_52,_53,_54,_55,_56,_57,_58,_59,_60,_61,_62,_63,_64,_65,_66,_67,_68,_69,_70,_71,_72,_73,_74,_75,_76,_77,_78,_79,_80,_81,_82,_83,_84,_85,_86,_87,_88,_89,_90,_91,_92,_93,_94,_95,_96,_97,_98,_99, N, ...) N
 #define REGISTER_RPC_FUNCTIONS_NARGS(...) REGISTER_RPC_FUNCTIONS_NARGS_(__VA_ARGS__, 99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
 #define REGISTER_RPC_FUNCTIONS_CONCAT(arg1,arg2)    REGISTER_RPC_FUNCTIONS_CONCAT1(arg1,arg2)
@@ -238,6 +222,22 @@
 #define REGISTER_RPC_FUNCTIONS_TUPLE(...)                                                                             \
     REGISTER_RPC_FUNCTIONS_TUPLE_(REGISTER_RPC_FUNCTIONS_NARGS(__VA_ARGS__),__VA_ARGS__)
 
+/**
+ * This macro automatically generates a register_functions() method for a Derecho
+ * Replicated Object. Example usage for a class Thing with methods foo() and bar():
+ *
+ * REGISTER_RPC_FUNCTIONS(Thing, ORDERED_TARGETS(foo1), P2P_TARGETS(bar1,bar2), ORDERED_TARGETS(foo2))
+ *
+ * The macro invocation belongs in your class definition, in the "public:" section.
+ *
+ * @param name  The name of the class that is being declared as a Replicated Object
+ * @param args  Each element of the args is either an invocation of the ORDERED_TARGETS macro containing the names of
+ *              each class method that should be callable by an ordered send, or an invocation of the P2P_TARGETS macro
+ *              containing the names of each class method that should be callable by a P2P send. Please note that the
+ *              number of arguments should not exceed 99, which, however can be extended using the above
+ *              REGISTER_RPC_FUNCTIONS_TUPLE_n macros.
+ *
+ */
 #define REGISTER_RPC_FUNCTIONS(name, args...)                                                                         \
     static auto register_functions() {                                                                                \
         if constexpr(derecho::rpc::well_formed_macro(#args)) {                                                        \
@@ -250,6 +250,11 @@
         }                                                                                                             \
     }
 
+/**
+ * This macro is similar to REGISTER_RPC_FUNCTIONS with a hidden P2P call called 'notify', which enables the server
+ * side notification to an external client. Please make sure the user provided subgroup type definition should derive
+ * derecho::NotificationSupport to use REGISTER_RPC_FUNCTIONS_WITH_NOTIFICATION.
+ */
 #define REGISTER_RPC_FUNCTIONS_WITH_NOTIFICATION(name, args...)                                                       \
     virtual void notify(const derecho::NotificationMessage& msg) const override {                                     \
         derecho::NotificationSupport::notify(msg);                                                                    \
