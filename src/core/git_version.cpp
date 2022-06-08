da326757cd253429c51ad4baefda3e35bf1803ca
Edward Tremel <high.order.bits@gmail.com>
2022-06-08 16:48:33 -0400

    Cleaned up CMake files
    After a few more hours of digging through CMake documentation and
    examples I realized that the CMakeLists.txt files in each subdirectory
    should not have their own CMAKE_REQUIRED_VERSION and project()
    incantations unless they are completely stand-alone projects that can be
    built without the parent directory. Also, the root CMakeLists should use
    the GNUInstallDirs module instead of manually setting
    CMAKE_INSTALL_LIBDIR and similar variables, and once this is done, the
    install(DIRECTORY) command can use the "TYPE INCLUDE" option to install
    include/derecho/ in the "standard" way.

--- src/core/git_version.cpp
+++ src/core/git_version.cpp
@@ -13,8 +13,8 @@ namespace derecho {
 const int MAJOR_VERSION = 2;
 const int MINOR_VERSION = 3;
 const int PATCH_VERSION = 0;
-const int COMMITS_AHEAD_OF_VERSION = 00;
+const int COMMITS_AHEAD_OF_VERSION = 2;
 const char* VERSION_STRING = "2.3.0";
-const char* VERSION_STRING_PLUS_COMMITS = "2.3.0+0";
+const char* VERSION_STRING_PLUS_COMMITS = "2.3.0+2";
 
 }
