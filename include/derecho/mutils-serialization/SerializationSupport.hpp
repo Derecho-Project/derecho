Derecho-Project/derecho

commit 4b402508b6d45b96acb990476b179d9cba7fc39c

separate STL's post_object() and to_bytes() implementation.

include/derecho/mutils-serialization/SerializationSupport.hpp
@@ -768,60 +768,135 @@ std::size_t to_bytes(const T& t, uint8_t* v) {
     return sizeof(T);
 }
 
-std::size_t to_bytes(const std::vector<bool>& vec, uint8_t* v);
-
 template <typename T>
-std::size_t to_bytes(const std::vector<T>& vec, uint8_t* v) {
+std::size_t to_bytes(const std::vector<T>& vec, uint8_t* buffer) {
+    /*
+     * we avoid using post_object for serialization to allow the "delayed instantiated object" mechanism
+     *
     auto size = bytes_size(vec);
     std::size_t index = 0;
     post_object(post_to_buffer(index, v), vec);
     return size;
+    */
+    int vector_size = vec.size();
+    std::size_t bsize = to_bytes(vector_size, buffer);
+    for (const auto& e: vec) {
+        bsize += to_bytes(e,buffer+bsize);
+    }
+    return bsize;
 }
 
 template <typename T>
 std::size_t to_bytes(const std::list<T>& list, uint8_t* buffer) {
+    /*
+     * we avoid using post_object for serialization to allow the "delayed instantiated object" mechanism
+     *
     auto size = bytes_size(list);
     std::size_t offset = 0;
     post_object(post_to_buffer(offset, buffer), list);
     return size;
+    */
+    int list_size = list.size();
+    std::size_t bsize = to_bytes(list_size, buffer);
+    for (const auto& e: list) {
+        bsize += to_bytes(e,buffer+bsize);
+    }
+    return bsize;
 }
 
 template <typename T, typename V>
 std::size_t to_bytes(const std::pair<T, V>& pair, uint8_t* buffer) {
+    /*
+     * we avoid using post_object for serialization to allow the "delayed instantiated object" mechanism
+     *
     std::size_t index = 0;
     post_object(post_to_buffer(index, buffer), pair);
     return bytes_size(pair);
+    */
+    std::size_t bsize = 0;
+    bsize += to_bytes(pair.first,buffer+bsize);
+    bsize += to_bytes(pair.second,buffer+bsize);
+    return bsize;
+}
+
+template <typename T>
+void to_bytes_helper1(uint8_t* buffer,std::size_t& offset,const T& t) {
+    offset = to_bytes(t,buffer+offset);
+}
+
+template <typename... T>
+std::size_t to_bytes_helper(uint8_t* buffer,const T&... t) {
+    std::size_t bsize = 0;
+    (to_bytes_helper1(buffer,bsize,t), ...);
+    return bsize;
 }
 
 template <typename... T>
 std::size_t to_bytes(const std::tuple<T...>& tuple, uint8_t* buffer) {
+    /*
+     * we avoid using post_object for serialization to allow the "delayed instantiated object" mechanism
+     *
     std::size_t index = 0;
     post_object(post_to_buffer(index, buffer), tuple);
     return bytes_size(tuple);
+    */
+    return std::apply([buffer](T... args){return to_bytes_helper(buffer,args...);}, tuple);
 }
 
 template <typename T>
-std::size_t to_bytes(const std::set<T>& s, uint8_t* _v) {
+std::size_t to_bytes(const std::set<T>& s, uint8_t* buffer) {
+    /*
+     * we avoid using post_object for serialization to allow the "delayed instantiated object" mechanism
+     *
     std::size_t index = 0;
     auto size = bytes_size(s);
     post_object(post_to_buffer(index, _v), s);
     return size;
+    */
+    int set_size = s.size();
+    std::size_t bsize = to_bytes(set_size,buffer);
+    for (const auto& e: s) {
+        bsize += to_bytes(e, buffer+bsize);
+    }
+    return bsize;
 }
 
 template <typename K, typename V>
 std::size_t to_bytes(const std::map<K, V>& m, uint8_t* buffer) {
+    /*
+     * we avoid using post_object for serialization to allow the "delayed instantiated object" mechanism
+     *
     std::size_t index = 0;
     std::size_t size = bytes_size(m);
     post_object(post_to_buffer(index, buffer), m);
     return size;
+    */
+    int map_size = m.size();
+    std::size_t bsize = to_bytes(map_size,buffer);
+    for (const auto& e: m) {
+        bsize += to_bytes(e.first,buffer+bsize);
+        bsize += to_bytes(e.second,buffer+bsize);
+    }
+    return bsize;
 }
 
 template <typename K, typename V>
 std::size_t to_bytes(const std::unordered_map<K, V>& m, uint8_t* buffer) {
+    /*
+     * we avoid using post_object for serialization to allow the "delayed instantiated object" mechanism
+     *
     std::size_t index = 0;
     std::size_t size = bytes_size(m);
     post_object(post_to_buffer(index, buffer), m);
     return size;
+    */
+    int map_size = m.size();
+    std::size_t bsize = to_bytes(map_size,buffer);
+    for (const auto& e: m) {
+        bsize += to_bytes(e.first,buffer+bsize);
+        bsize += to_bytes(e.second,buffer+bsize);
+    }
+    return bsize;
 }
 
 // end to_bytes section

src/mutils-serialization/CMakeLists.txt
@@ -9,3 +9,8 @@ target_include_directories(mutils-serialization PRIVATE
   $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>)
 
 add_custom_target(format_mutils-serialization clang-format-3.8 -i *.cpp *.hpp)
+
+add_executable(test test_new_deserialization.cpp SerializationSupport.cpp)
+target_include_directories(test PRIVATE 
+  $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+  $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>)

src/mutils-serialization/test_new_deserialization.cpp
@@ -1,4 +1,5 @@
-#include "SerializationSupport.hpp"
+#include <derecho/mutils-serialization/SerializationSupport.hpp>
+#include <functional>
 
 using namespace mutils;
 
@@ -8,19 +9,23 @@ int main() {
 	uint8_t c[size];
 	bzero(c,size);
 	to_bytes(v,c);
-	deserialize_and_run<std::vector<int> >(nullptr,c,[&](std::vector<int>& v2){
+/*
+    std::function<void (const std::vector<int>&)> fun1 = [&](const std::vector<int>& v2){
 			assert(v2 == v);
-		});
-
-	deserialize_and_run<int >(nullptr,c,[&](int& v2){
+		};
+	deserialize_and_run(nullptr,c,fun1);
+*/
+	deserialize_and_run(nullptr,c,[&](const int& v2) {
 			assert(v2 == v.size());
+            return;
 		});
 
-	deserialize_and_run(nullptr,c,[&](int& size, int& v0, int& v1, int &v2, int &v3){
+	deserialize_and_run(nullptr,c,[&](const int& size, const int& v0, const int& v1, const int &v2, const int &v3){
 			assert(size == v.size());
 			assert(v0 == v.at(0));
 			assert(v1 == v.at(1));
 			assert(v2 == v.at(2));
 			assert(v3 == v.at(3));
+            return;
 		});
 }

commit 3fc7a15250abf8bca55c9738b197519da52d1d74

Bugfix for mutils::to_bytes<std::tuple<>>()
This bug is introduced by myself in commit 4b402508b6d45b96acb990476b179d9cba7fc39c.

include/derecho/mutils-serialization/SerializationSupport.hpp
@@ -828,7 +828,7 @@ std::size_t to_bytes(const std::pair<T, V>& pair, uint8_t* buffer) {
 
 template <typename T>
 void to_bytes_helper1(uint8_t* buffer,std::size_t& offset,const T& t) {
-    offset = to_bytes(t,buffer+offset);
+    offset += to_bytes(t,buffer+offset);
 }
 
 template <typename... T>