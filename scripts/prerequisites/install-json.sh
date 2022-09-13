c5eea07cfffa41b0a98f2b5a19ae361fd5bc3ee4
Weijia Song <songweijia@gmail.com>
2022-09-12 23:27:55 -0400

    minor fix for build with ubuntu 22.04 + gcc 11

--- scripts/prerequisites/install-json.sh
+++ scripts/prerequisites/install-json.sh
@@ -12,7 +12,7 @@ WORKPATH=`mktemp -d`
 cd ${WORKPATH}
 git clone https://github.com/nlohmann/json.git
 cd json
-git checkout v3.9.1
+git checkout v3.11.1
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} .
 make -j `lscpu | grep "^CPU(" | awk '{print $2}'`
 make install
--- scripts/prerequisites/install-mutils.sh
+++ scripts/prerequisites/install-mutils.sh
@@ -2,6 +2,7 @@
 set -eu
 export TMPDIR=/var/tmp
 INSTALL_PREFIX="/usr/local"
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 if [[ $# -gt 0 ]]; then
     INSTALL_PREFIX=$1
 fi
@@ -13,7 +14,9 @@ cd ${WORKPATH}
 git clone https://github.com/mpmilano/mutils.git
 cd mutils
 # git checkout f1bdc9f2224a6a85d91acd2703e346c52f0f1a42
-git checkout 2b6f80b60f2b1695bd27dce8dd30cbc3170d7290
+# git checkout 2b6f80b60f2b1695bd27dce8dd30cbc3170d7290
+git checkout d53e7fa7cd5146fd2995a7f05d5d01d03d872e67
+git apply ${SCRIPTPATH}/mutils.patch
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ..
