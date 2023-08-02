c05cee5fb5a8011cafdc2782d42d84d2c64c4708
Edward Tremel <high.order.bits@gmail.com>
2023-08-02 14:09:16 -0400

    Updated derechoConfig.cmake to forward public dependencies
    According to the CMake documentation, any "modern" (CMake import target)
    dependency that is linked publicly to a library will not automatically
    be "forwarded" to downstream consumers of that library unless you write
    these incantations in the Config.cmake file. This results in strange
    errors like "target cascade depends on target mutils::mutils-containers
    but it was not found. Are you missing a find_package() call?"

--- derechoConfig.cmake
+++ derechoConfig.cmake
@@ -1,5 +1,14 @@
 @PACKAGE_INIT@
 
+include(CMakeFindDependencyMacro)
+# For dependencies that are properly packaged the "modern" way (exporting targets),
+# we need to ensure these targets are forwarded to downstream projects that depend on Derecho
+find_dependency(mutils)
+find_dependency(mutils-containers)
+find_dependency(spdlog 1.3.1)
+find_dependency(OpenSSL 1.1.1)
+find_dependency(nlohmann_json 3.9.0)
+
 set_and_check(derecho_INCLUDE_DIRS "@PACKAGE_CMAKE_INSTALL_INCLUDEDIR@")
 set(derecho_LIBRARIES "-L@PACKAGE_CMAKE_INSTALL_LIBDIR@ -lderecho -pthread")
 include("${CMAKE_CURRENT_LIST_DIR}/derechoTargets.cmake")
